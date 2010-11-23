8216206a34ba15b9db5f58476b5850c16f212112
Jenny Gurney <gurneyj@wustl.edu>
2010-11-22 21:19:39 -0600

    Kevin Archie's Uploader Applet update
    Former-commit-id: 22189e21d504e4f9edd6333dd00c980ba27de4e0

--- plugin-resources/webapp/xnat/java/org/nrg/xnat/turbine/modules/screens/UploadApplet.java
+++ plugin-resources/webapp/xnat/java/org/nrg/xnat/turbine/modules/screens/UploadApplet.java
@@ -1,7 +1,6 @@
 // Copyright 2010 Washington University School of Medicine All Rights Reserved
 package org.nrg.xnat.turbine.modules.screens;
 
-import org.apache.log4j.Logger;
 import org.apache.turbine.util.RunData;
 import org.apache.velocity.context.Context;
 import org.nrg.xdat.om.XnatExperimentdata;
@@ -9,9 +8,11 @@ import org.nrg.xdat.turbine.modules.screens.SecureScreen;
 import org.nrg.xdat.turbine.utils.TurbineUtils;
 import org.nrg.xft.XFTItem;
 import org.nrg.xnat.turbine.utils.ArcSpecManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class DICOMUploadApplet extends SecureScreen {
-	static Logger logger = Logger.getLogger(DICOMUploadApplet.class);
+public class UploadApplet extends SecureScreen {
+	static Logger logger = LoggerFactory.getLogger(UploadApplet.class);
 	
 	@Override
 	protected void doBuildTemplate(RunData data, Context context) throws Exception {

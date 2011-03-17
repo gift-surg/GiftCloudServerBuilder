537d88ff2c5af925a45588e4d347e4fb664d7a0c
Tim Olsen <olsent@wustl.edu>
2011-03-17 14:48:47 -0500

    Minor refactor to remove findbugs issue.
    Former-commit-id: 80d4af05247868cf09dfef1c586457e04814dfa2

--- plugin-resources/webapp/xnat/java/org/nrg/xnat/restlet/resources/SecureResource.java
+++ plugin-resources/webapp/xnat/java/org/nrg/xnat/restlet/resources/SecureResource.java
@@ -398,24 +398,19 @@ public abstract class SecureResource extends Resource {
 					List<FileItem> items = upload.parseRequest(this							.getRequest());
 				
 					for (FileItem fi : items) {
-						String fileName = fi.getName();
-						if (fileName.indexOf("\\") > -1) {
-							fileName.substring(fileName.indexOf("\\") + 1);
-						}
-
 						if (fi.getName().endsWith(".xml")) {
-				SAXReader reader = new SAXReader(user);
+							SAXReader reader = new SAXReader(user);
 							if (item != null) {
 								reader.setTemplate(item);
 							}
 							try {
 								item = reader.parse(fi.getInputStream());
 
-				if(!reader.assertValid()){
-					throw reader.getErrors().get(0);
-				}
+								if(!reader.assertValid()){
+									throw reader.getErrors().get(0);
+								}
 								if (XFT.VERBOSE) {
-	                System.out.println("Loaded XML Item:" + item.getProperName());
+									System.out.println("Loaded XML Item:" + item.getProperName());
 								}
 								if (item != null) {
 									completeDocument = true;

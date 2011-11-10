35ba55213ba883b0194d1182ab3a0488e1dbbd37
Rick Herrick <rick.herrick@wustl.edu>
2011-11-10 14:51:16 -0600

    Completed configuration and project mix-in structures, as well as restlet extension integration.
    Former-commit-id: 67e8a31f4aeb8fa8f64b28c8b93e6171fa9a4c42

---
+++ plugin-resources/webapp/xnat/java/org/nrg/xnat/restlet/extensions/VerifyExtensionsRestlet.java
@@ -0,0 +1,43 @@
+/*
+ * (C) 2011 Washington University School of Medicine
+ * All Rights Reserved
+ * Released under the Simplified BSD License
+ */
+
+/**
+ * VerifyExtensionsRestlet
+ * Created on 11/9/11 by rherri01
+ */
+package org.nrg.xnat.restlet.extensions;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.nrg.xnat.restlet.XnatRestlet;
+import org.nrg.xnat.restlet.resources.SecureResource;
+import org.restlet.Context;
+import org.restlet.data.MediaType;
+import org.restlet.data.Request;
+import org.restlet.data.Response;
+import org.restlet.resource.Representation;
+import org.restlet.resource.ResourceException;
+import org.restlet.resource.StringRepresentation;
+import org.restlet.resource.Variant;
+
+@XnatRestlet("/services/extensions/verify")
+public class VerifyExtensionsRestlet extends SecureResource {
+    private static final Log _log = LogFactory.getLog(VerifyExtensionsRestlet.class);
+
+    public VerifyExtensionsRestlet(Context context, Request request, Response response) {
+        super(context, request, response);
+        this.getVariants().add(new Variant(MediaType.ALL));
+    }
+
+    @Override
+    public Representation represent(Variant variant) throws ResourceException {
+        if (_log.isDebugEnabled()) {
+            _log.debug("Passing a representation of the verify extensions restlet.");
+        }
+
+        return new StringRepresentation("This is to verify that dynamically configured restlets are functioning properly.");
+    }
+}

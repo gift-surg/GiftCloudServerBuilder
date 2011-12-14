dba0157c4603407a78b0b9887154b9c12e0bb787
Kevin A. Archie <karchie@wustl.edu>
2011-12-14 10:53:29 -0600

    added CompositeDicomObjectIdentifier for easier DICOM id customization
    Former-commit-id: 0d2dc368237ef7ba88dd3d38e08f7951109c1fe9

--- plugin-resources/webapp/xnat/java/org/nrg/dcm/id/ClassicDicomObjectIdentifier.java
+++ plugin-resources/webapp/xnat/java/org/nrg/dcm/id/ClassicDicomObjectIdentifier.java
@@ -3,96 +3,47 @@
  */
 package org.nrg.dcm.id;
 
-import java.util.SortedSet;
+import java.util.List;
 import java.util.regex.Pattern;
 
-import javax.inject.Provider;
-
-import org.dcm4che2.data.DicomObject;
 import org.dcm4che2.data.Tag;
-import org.nrg.dcm.ChainExtractor;
 import org.nrg.dcm.ContainedAssignmentExtractor;
 import org.nrg.dcm.Extractor;
 import org.nrg.dcm.TextExtractor;
-import org.nrg.xdat.om.XnatProjectdata;
-import org.nrg.xdat.security.XDATUser;
-import org.nrg.xnat.DicomObjectIdentifier;
-import org.nrg.xnat.Labels;
 
-import com.google.common.collect.ImmutableSortedSet;
+import com.google.common.collect.ImmutableList;
 
 /**
  * DicomObjectIdentifier that implements the standard XNAT 1.4/1.5 object identification
  * @author Kevin A. Archie <karchie@wustl.edu>
  *
  */
-public class ClassicDicomObjectIdentifier implements
-        DicomObjectIdentifier<XnatProjectdata> {
-    private static final DicomProjectIdentifier projectID = new Xnat15DicomProjectIdentifier();
-    
-    private static final Extractor aaExtractor = new ChainExtractor(
-            new ContainedAssignmentExtractor(Tag.PatientComments, "AA", Pattern.CASE_INSENSITIVE),
-            new ContainedAssignmentExtractor(Tag.StudyComments, "AA", Pattern.CASE_INSENSITIVE));
-
-    private static final Extractor sessionExtractor = new ChainExtractor(
-            new ContainedAssignmentExtractor(Tag.PatientComments, "Session", Pattern.CASE_INSENSITIVE),
-            new ContainedAssignmentExtractor(Tag.StudyComments, "Session", Pattern.CASE_INSENSITIVE),
-            new TextExtractor(Tag.PatientID));
-
-    private static final Extractor subjectExtractor = new ChainExtractor(
-            new ContainedAssignmentExtractor(Tag.PatientComments, "Subject", Pattern.CASE_INSENSITIVE),
-            new ContainedAssignmentExtractor(Tag.StudyComments, "Subject", Pattern.CASE_INSENSITIVE),
-            new TextExtractor(Tag.PatientName));
-
-    private static SortedSet<Integer> tags;
+public class ClassicDicomObjectIdentifier extends CompositeDicomObjectIdentifier {
+    private static final ImmutableList<Extractor> aaExtractors, sessionExtractors, subjectExtractors;
     static {
-        ImmutableSortedSet.Builder<Integer> builder = ImmutableSortedSet.naturalOrder();
-        builder.addAll(projectID.getTags());
-        builder.addAll(aaExtractor.getTags());
-        builder.addAll(sessionExtractor.getTags());
-        builder.addAll(subjectExtractor.getTags());
-        tags = builder.build();
-    }
-    
-    private XDATUser user;
-    
-    public XnatProjectdata getProject(final DicomObject o) {
-        return projectID.apply(user, o);
-    }
-    
-    public String getSessionLabel(final DicomObject o) {
-        return Labels.toLabelChars(sessionExtractor.extract(o));
-    }
-    
-    public String getSubjectLabel(final DicomObject o) {
-        return Labels.toLabelChars(subjectExtractor.extract(o));
+        final ImmutableList.Builder<Extractor> aab = new ImmutableList.Builder<Extractor>();
+        aab.add(new ContainedAssignmentExtractor(Tag.PatientComments, "AA", Pattern.CASE_INSENSITIVE),
+                new ContainedAssignmentExtractor(Tag.StudyComments, "AA", Pattern.CASE_INSENSITIVE));
+        aaExtractors = aab.build();
+
+        final ImmutableList.Builder<Extractor> sessb = new ImmutableList.Builder<Extractor>();
+        sessb.add(new ContainedAssignmentExtractor(Tag.PatientComments, "Session", Pattern.CASE_INSENSITIVE),
+                new ContainedAssignmentExtractor(Tag.StudyComments, "Session", Pattern.CASE_INSENSITIVE),
+                new TextExtractor(Tag.PatientID));
+        sessionExtractors = sessb.build();
+
+        final ImmutableList.Builder<Extractor> subjb = new ImmutableList.Builder<Extractor>();
+        subjb.add(new ContainedAssignmentExtractor(Tag.PatientComments, "Subject", Pattern.CASE_INSENSITIVE),
+                new ContainedAssignmentExtractor(Tag.StudyComments, "Subject", Pattern.CASE_INSENSITIVE),
+                new TextExtractor(Tag.PatientName));
+        subjectExtractors = subjb.build();
     }
 
-    public SortedSet<Integer> getTags() {
-        return tags;
-    }
-    
-    private static final Pattern TRUE = Pattern.compile("t(?:rue)?", Pattern.CASE_INSENSITIVE);
-    private static final Pattern FALSE = Pattern.compile("f(?:alse)?", Pattern.CASE_INSENSITIVE);
+    public static final List<Extractor> getAAExtractors() { return aaExtractors; }
+    public static final List<Extractor> getSessionExtractors() { return sessionExtractors; }
+    public static final List<Extractor> getSubjectExtractors() { return subjectExtractors; }
 
-    public Boolean requestsAutoarchive(DicomObject o) {
-        final String aa = aaExtractor.extract(o);
-        if (null == aa) {
-            return null;
-        } else if (TRUE.matcher(aa).matches()) {
-            return true;
-        } else if (FALSE.matcher(aa).matches()) {
-            return false;
-        } else {
-            return null;
-        }
-    }
-    
-    public void setUser(final XDATUser user) {
-        this.user = user;
-    }
-    
-    public void setUserProvider(final Provider<XDATUser> user) {
-        this.user = user.get();
+    public ClassicDicomObjectIdentifier() {
+        super(new Xnat15DicomProjectIdentifier(), sessionExtractors, subjectExtractors, aaExtractors);
     }
 }
--- plugin-resources/webapp/xnat/java/org/nrg/xnat/restlet/actions/importer/ImporterHandlerA.java
+++ plugin-resources/webapp/xnat/java/org/nrg/xnat/restlet/actions/importer/ImporterHandlerA.java
@@ -23,91 +23,100 @@ import org.nrg.xnat.restlet.actions.SessionImporter;
 import org.nrg.xnat.restlet.actions.XarImporter;
 import org.nrg.xnat.restlet.util.FileWriterWrapperI;
 import org.nrg.xnat.turbine.utils.PropertiesHelper;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 
 @SuppressWarnings({ "rawtypes", "unchecked" })
 public abstract class ImporterHandlerA  extends StatusProducer implements Callable<List<String>>{
 
 
-	public ImporterHandlerA(final Object listenerControl, final XDATUser u,  final FileWriterWrapperI fw, final Map<String,Object> params) {
-		super((listenerControl==null)?u:listenerControl);
-	}
-	
-	public abstract List<String> call() throws ClientException, ServerException;
-	
-	static Logger logger = Logger.getLogger(ImporterHandlerA.class);
+    public ImporterHandlerA(final Object listenerControl, final XDATUser u,  final FileWriterWrapperI fw, final Map<String,Object> params) {
+        super((listenerControl==null)?u:listenerControl);
+    }
 
-	public static final String IMPORT_HANDLER_ATTR = "import-handler";
-	
-	public static String SESSION_IMPORTER="SI";
-	public static String XAR_IMPORTER="XAR";
-	public static String GRADUAL_DICOM_IMPORTER="gradual-DICOM";
-	public static String DICOM_ZIP_IMPORTER="DICOM-zip";
-	
-	static String DEFAULT_HANDLER=SESSION_IMPORTER;
-	final static Map<String,Class<? extends ImporterHandlerA>> IMPORTERS=new HashMap<String,Class<? extends ImporterHandlerA>>();
+    public abstract List<String> call() throws ClientException, ServerException;
 
-	private static final String PROP_OBJECT_IDENTIFIER = "org.nrg.import.handler.impl";
-	private static final String IMPORTER_PROPERTIES = "importer.properties";
-	private static final String CLASS_NAME = "className";
-	private static final String[] PROP_OBJECT_FIELDS = new String[]{CLASS_NAME};
-	static{
-		//EXAMPLE PROPERTIES FILE 
-		//org.nrg.import.handler=NIFTI
-		//org.nrg.import.handler.impl.NIFTI.className=org.nrg.import.handler.CustomNiftiImporter
-		try {
-			IMPORTERS.putAll((new PropertiesHelper<ImporterHandlerA>()).buildClassesFromProps(IMPORTER_PROPERTIES, PROP_OBJECT_IDENTIFIER, PROP_OBJECT_FIELDS, CLASS_NAME));
-						
-			if(!IMPORTERS.containsKey(SESSION_IMPORTER))IMPORTERS.put(SESSION_IMPORTER, SessionImporter.class);
-			if(!IMPORTERS.containsKey(XAR_IMPORTER))IMPORTERS.put(XAR_IMPORTER, XarImporter.class);
-			if(!IMPORTERS.containsKey(GRADUAL_DICOM_IMPORTER))IMPORTERS.put(GRADUAL_DICOM_IMPORTER, GradualDicomImporter.class);
-			if(!IMPORTERS.containsKey(DICOM_ZIP_IMPORTER))IMPORTERS.put(DICOM_ZIP_IMPORTER, DicomZipImporter.class);
-		} catch (Exception e) {
-			logger.error("",e);
-		}
-	}
-	
-	public static ImporterHandlerA buildImporter(String format,final Object uID, final XDATUser u, final FileWriterWrapperI fi, Map<String,Object> params) throws IllegalArgumentException, InstantiationException, IllegalAccessException, InvocationTargetException, SecurityException, NoSuchMethodException, ImporterNotFoundException {
-		if(StringUtils.isEmpty(format)){
-			format=DEFAULT_HANDLER;
-		}
-		
-		Class<? extends ImporterHandlerA> importerImpl=IMPORTERS.get(format);
-		if(importerImpl==null){
-			
-			throw new ImporterNotFoundException("Unknown importer implementation specified: " + format,new IllegalArgumentException());
-		}
-		
-		final Constructor con=importerImpl.getConstructor(Object.class, XDATUser.class, FileWriterWrapperI.class, Map.class);
-		final ImporterHandlerA handler = (ImporterHandlerA)con.newInstance(uID, u, fi, params);
-		
-		/* Abuse Spring to inject some additional parameters. Please fix this. */
-		if (GradualDicomImporter.class.equals(importerImpl)) {
-		    final ContextService context = XDAT.getContextService();
-		    final GradualDicomImporter gdi = (GradualDicomImporter)handler;
-		    gdi.setIdentifier(context.getBean("dicomObjectIdentifier", DicomObjectIdentifier.class));
-		    final DicomFileNamer namer = context.getBean("dicomFileNamer", DicomFileNamer.class);
-		    if (null != namer) {
-		        gdi.setNamer(namer);
-		    }
-		} else if (DicomZipImporter.class.equals(importerImpl)) {
-		    final ContextService context = XDAT.getContextService();
-		    final DicomZipImporter dzi = (DicomZipImporter)handler;
-		    dzi.setIdentifier(context.getBean("dicomObjectIdentifier", DicomObjectIdentifier.class));
-		    final DicomFileNamer namer = context.getBean("dicomFileNamer", DicomFileNamer.class);
-		    if (null != namer) {
-		        dzi.setNamer(namer);
-		    }
-		}
-		
-		return handler;
-	
-	}
-	
-	/**
-	 * This method was added to allow other developers to manually add importers to the list, without adding a configuration file.  However, this would some how need to be done before the import is executed (maybe as a servlet?).
-	 * @return
-	 */
-	public static Map<String,Class<? extends ImporterHandlerA>> getImporters(){
-		return IMPORTERS;
-	}
+    static Logger logger = Logger.getLogger(ImporterHandlerA.class);
+
+    public static final String IMPORT_HANDLER_ATTR = "import-handler";
+
+    public static String SESSION_IMPORTER="SI";
+    public static String XAR_IMPORTER="XAR";
+    public static String GRADUAL_DICOM_IMPORTER="gradual-DICOM";
+    public static String DICOM_ZIP_IMPORTER="DICOM-zip";
+
+    static String DEFAULT_HANDLER=SESSION_IMPORTER;
+    final static Map<String,Class<? extends ImporterHandlerA>> IMPORTERS=new HashMap<String,Class<? extends ImporterHandlerA>>();
+
+    private static final String PROP_OBJECT_IDENTIFIER = "org.nrg.import.handler.impl";
+    private static final String IMPORTER_PROPERTIES = "importer.properties";
+    private static final String CLASS_NAME = "className";
+    private static final String[] PROP_OBJECT_FIELDS = new String[]{CLASS_NAME};
+    static{
+        //EXAMPLE PROPERTIES FILE 
+        //org.nrg.import.handler=NIFTI
+        //org.nrg.import.handler.impl.NIFTI.className=org.nrg.import.handler.CustomNiftiImporter
+        try {
+            IMPORTERS.putAll((new PropertiesHelper<ImporterHandlerA>()).buildClassesFromProps(IMPORTER_PROPERTIES, PROP_OBJECT_IDENTIFIER, PROP_OBJECT_FIELDS, CLASS_NAME));
+
+            if(!IMPORTERS.containsKey(SESSION_IMPORTER))IMPORTERS.put(SESSION_IMPORTER, SessionImporter.class);
+            if(!IMPORTERS.containsKey(XAR_IMPORTER))IMPORTERS.put(XAR_IMPORTER, XarImporter.class);
+            if(!IMPORTERS.containsKey(GRADUAL_DICOM_IMPORTER))IMPORTERS.put(GRADUAL_DICOM_IMPORTER, GradualDicomImporter.class);
+            if(!IMPORTERS.containsKey(DICOM_ZIP_IMPORTER))IMPORTERS.put(DICOM_ZIP_IMPORTER, DicomZipImporter.class);
+        } catch (Exception e) {
+            logger.error("",e);
+        }
+    }
+
+    public static ImporterHandlerA buildImporter(String format,final Object uID, final XDATUser u, final FileWriterWrapperI fi, Map<String,Object> params) throws IllegalArgumentException, InstantiationException, IllegalAccessException, InvocationTargetException, SecurityException, NoSuchMethodException, ImporterNotFoundException {
+        if(StringUtils.isEmpty(format)){
+            format=DEFAULT_HANDLER;
+        }
+
+        Class<? extends ImporterHandlerA> importerImpl=IMPORTERS.get(format);
+        if(importerImpl==null){
+
+            throw new ImporterNotFoundException("Unknown importer implementation specified: " + format,new IllegalArgumentException());
+        }
+
+        final Constructor con=importerImpl.getConstructor(Object.class, XDATUser.class, FileWriterWrapperI.class, Map.class);
+        final ImporterHandlerA handler = (ImporterHandlerA)con.newInstance(uID, u, fi, params);
+
+        /* Abuse Spring to inject some additional parameters. Please fix this. */
+        if (GradualDicomImporter.class.equals(importerImpl)) {
+            final ContextService context = XDAT.getContextService();
+            final GradualDicomImporter gdi = (GradualDicomImporter)handler;
+            gdi.setIdentifier(context.getBean("dicomObjectIdentifier", DicomObjectIdentifier.class));
+            try {
+                final DicomFileNamer namer = context.getBean("dicomFileNamer", DicomFileNamer.class);
+                if (null != namer) {
+                    gdi.setNamer(namer);
+                }
+            } catch (NoSuchBeanDefinitionException e) {
+                logger.debug("no DicomFileNamer specified", e);
+            }
+        } else if (DicomZipImporter.class.equals(importerImpl)) {
+            final ContextService context = XDAT.getContextService();
+            final DicomZipImporter dzi = (DicomZipImporter)handler;
+            dzi.setIdentifier(context.getBean("dicomObjectIdentifier", DicomObjectIdentifier.class));
+            try {
+                final DicomFileNamer namer = context.getBean("dicomFileNamer", DicomFileNamer.class);
+                if (null != namer) {
+                    dzi.setNamer(namer);
+                }
+            } catch (NoSuchBeanDefinitionException e) {
+                logger.debug("no DicomFileNamer specified", e);
+            }
+        }
+
+        return handler;
+
+    }
+
+    /**
+     * This method was added to allow other developers to manually add importers to the list, without adding a configuration file.  However, this would some how need to be done before the import is executed (maybe as a servlet?).
+     * @return
+     */
+    public static Map<String,Class<? extends ImporterHandlerA>> getImporters(){
+        return IMPORTERS;
+    }
 }

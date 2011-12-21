55b852a4225d81114c614621e5ee8be1998e916b
Timothy R. Olsen <olsent@wustl.edu>
2011-12-21 14:41:39 -0600

    Add Screening Assessment for use in previous changeset, and clean up a few minor issues.
    Former-commit-id: 83edfc043ae616450cdd59e481adbf1f0776d256

---
+++ plugin-resources/webapp/xnat/java/org/nrg/xdat/om/base/BaseScrScreeningassessment.java
@@ -0,0 +1,76 @@
+/*
+ * GENERATED FILE
+ * Created on Wed Dec 14 16:25:52 CST 2011
+ *
+ */
+package org.nrg.xdat.om.base;
+import org.nrg.xdat.model.ScrScreeningscandataI;
+import org.nrg.xdat.model.ValProtocoldataScanCheckI;
+import org.nrg.xdat.om.ScrScreeningscandata;
+import org.nrg.xdat.om.ValProtocoldataScanCheck;
+import org.nrg.xdat.om.XnatQcassessmentdataScan;
+import org.nrg.xdat.om.base.auto.*;
+import org.nrg.xft.*;
+import org.nrg.xft.security.UserI;
+import org.nrg.xnat.scanAssessors.ScanAssessorI;
+import org.nrg.xnat.scanAssessors.ScanAssessorScanI;
+
+import java.util.*;
+
+/**
+ * @author XDAT
+ *
+ */
+@SuppressWarnings({"unchecked","rawtypes"})
+public abstract class BaseScrScreeningassessment extends AutoScrScreeningassessment implements ScanAssessorI {
+
+	public BaseScrScreeningassessment(ItemI item)
+	{
+		super(item);
+	}
+
+	public BaseScrScreeningassessment(UserI user)
+	{
+		super(user);
+	}
+
+	/*
+	 * @deprecated Use BaseScrScreeningassessment(UserI user)
+	 **/
+	public BaseScrScreeningassessment()
+	{}
+
+	public BaseScrScreeningassessment(Hashtable properties, UserI user)
+	{
+		super(properties,user);
+	}
+	
+	public ScrScreeningscandata getScanScreeningAssessment(String scanId){
+		ScrScreeningscandata rtn = null;
+		if (scanId == null) throw new NullPointerException("Expected a non-null value for the scan id input parameter");
+		List<ScrScreeningscandataI> scans = super.getScans_scan();
+		if (scans != null && scans.size() > 0) {
+			for (int i=0; i< scans.size(); i++) {
+				ScrScreeningscandata aScan = (ScrScreeningscandata)scans.get(i);
+				if (aScan.getImagescanId().equals(scanId)) {
+					rtn = aScan;
+					break;
+				}
+			}
+		}
+		if (rtn == null) throw new NullPointerException("Couldnt find screening assessment for scan id " + scanId);
+		return rtn;
+	}
+	
+	public ScanAssessorScanI getScanById(String id){
+		return this.getScanScreeningAssessment(id);
+	}
+	
+	public String getHeader(){
+		return "Screening Assess";
+	}
+
+	public int getPrecedence(){
+		return 1;
+	}
+}

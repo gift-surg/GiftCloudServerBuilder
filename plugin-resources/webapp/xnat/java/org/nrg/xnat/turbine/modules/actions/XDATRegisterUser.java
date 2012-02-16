49c7ad3b04ee705a60cbd9b08ec748fc3be4f8d4
NRG Admin <nrg-admin@nrg.wustl.edu>
2012-02-16 12:43:32 -0600

    Missed some raw calls to data.getParameters()
    Former-commit-id: 7fe80e4c54c8c6955dd0e9458ec83423edaf4570

--- plugin-resources/webapp/xnat/java/org/nrg/xnat/turbine/modules/actions/XDATRegisterUser.java
+++ plugin-resources/webapp/xnat/java/org/nrg/xnat/turbine/modules/actions/XDATRegisterUser.java
@@ -20,8 +20,8 @@ public class XDATRegisterUser extends org.nrg.xdat.turbine.modules.actions.XDATR
     static Logger logger = Logger.getLogger(XDATRegisterUser.class);
 
     public void directRequest(RunData data,Context context,XDATUser user) throws Exception{
-		String nextPage = data.getParameters().getString("nextPage","");
-		String nextAction = data.getParameters().getString("nextAction","");
+		String nextPage = (String)TurbineUtils.GetPassedParameter("nextPage",data);
+		String nextAction = (String)TurbineUtils.GetPassedParameter("nextAction",data);
 
         data.setScreenTemplate("Index.vm");
         
@@ -61,7 +61,7 @@ public class XDATRegisterUser extends org.nrg.xdat.turbine.modules.actions.XDATR
             msg +="<br>Lab: " + TurbineUtils.GetPassedParameter("lab", data);
         
         
-        String parID = data.getParameters().getString("par","");
+        String parID = (String)TurbineUtils.GetPassedParameter("par",data);
 		
 		if(!StringUtils.isEmpty(parID)){ 
 			ProjectAccessRequest par =ProjectAccessRequest.RequestPARById(Integer.valueOf(parID), null);
@@ -81,7 +81,7 @@ public class XDATRegisterUser extends org.nrg.xdat.turbine.modules.actions.XDATR
 			return true;
 		}
 		
-		String parID = data.getParameters().getString("par","");
+		String parID = (String)TurbineUtils.GetPassedParameter("par",data);
 		
 		if(!StringUtils.isEmpty(parID)){ 
 			ProjectAccessRequest par =ProjectAccessRequest.RequestPARById(Integer.valueOf(parID), null);

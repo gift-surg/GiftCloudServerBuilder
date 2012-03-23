f81a99ee5b599a7d66f5b7f93e10db7b4dd062cc
NRG Admin <nrg-admin@nrg.wustl.edu>
2012-03-23 12:07:51 -0500

    Simplification of the cookie setting logic, removed unnecessary inner class
    Former-commit-id: 992d8cd32585a4d8bdec1dbd435ed4c543308370

--- plugin-resources/webapp/xnat/java/org/nrg/xnat/restlet/util/UpdateExpirationCookie.java
+++ plugin-resources/webapp/xnat/java/org/nrg/xnat/restlet/util/UpdateExpirationCookie.java
@@ -17,28 +17,6 @@ import javax.servlet.http.HttpServletRequestWrapper;
 import javax.servlet.http.HttpServletResponse;
 
 public class UpdateExpirationCookie implements Filter {
-	static class RemoveCookieFromRequest extends HttpServletRequestWrapper{
-		private final int sessionIdleTime;
-		public RemoveCookieFromRequest (HttpServletRequest req, int sessionIdleTime) {
-			super(req);
-			this.sessionIdleTime = sessionIdleTime;
-		}
-		public Cookie[] getCookies() {
-			Cookie[] cookies = super.getCookies();
-			List<Cookie> newCookies = new ArrayList<Cookie>();
-			// cookies only come from a HTTP session
-			if (cookies != null) {
-				for (int i =0; i < cookies.length; i++) {
-					Cookie c = cookies[i];
-					if (c.getName().equals(UpdateExpirationCookie.name)){
-						Cookie d = UpdateExpirationCookie.makeCookie(sessionIdleTime);
-						c = d;
-					}
-				}
-			}
-			return newCookies.toArray(new Cookie[newCookies.size()]);
-		}
-	}
 	public static String name = "SESSION_EXPIRATION_TIME";
 	public static Cookie makeCookie (int sessionIdleTime) {
 		long start = System.currentTimeMillis();
@@ -57,11 +35,6 @@ public class UpdateExpirationCookie implements Filter {
 		HttpServletResponse hr = (HttpServletResponse) resp;
 		int sessionIdleTime = hq.getSession().getMaxInactiveInterval();
 		Cookie cookie = UpdateExpirationCookie.makeCookie(sessionIdleTime);
-//		RemoveCookieFromRequest rq = new RemoveCookieFromRequest(hq, sessionIdleTime);
-//		Cookie[] cookies = rq.getCookies();
-//		for (int i = 0; i < cookies.length; i++) {
-//			hr.addCookie(cookies[i]);
-//		}
 		hr.addCookie(cookie);
 		chain.doFilter(req,resp);
 	}

60e06fca78edfae855bc21cf00114ba54b9b8ec0
Rick Herrick <rick.herrick@wustl.edu>
2012-03-26 13:52:36 -0500

    Fixed annoying sample URL. Fixed content disposition header call.
    Former-commit-id: cf827664f53f833fd5db70c0a40f726f82e84934

--- plugin-resources/webapp/xnat/java/org/nrg/xnat/restlet/resources/SecureResource.java
+++ plugin-resources/webapp/xnat/java/org/nrg/xnat/restlet/resources/SecureResource.java
@@ -471,7 +471,7 @@ public abstract class SecureResource extends Resource {
 	public FileRepresentation representFile(File f,MediaType mt){
 		mt=buildMediaType(mt,f.getName());
 
-		this.setContentDisposition(String.format("attachment; filename=\"%s\";",f.getName()));
+		this.setContentDisposition(f.getName());
 
 		FileRepresentation fr= new FileRepresentation(f,mt);
 		fr.setModificationDate(new Date(f.lastModified()));

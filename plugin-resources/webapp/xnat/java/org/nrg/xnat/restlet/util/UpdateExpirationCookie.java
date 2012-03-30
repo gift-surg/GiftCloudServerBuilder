38930554f8d91d773148c714efe1573669a9b199
NRG Admin <nrg-admin@nrg.wustl.edu>
2012-03-30 16:56:59 -0500

    Change the counter to use the client system clock to calculate time left in the session and the server side cookie to tuple containing a flag and the maximum idle time
    Former-commit-id: cb0012aff8cc439b8b7dc26ac529af702b95157a

--- plugin-resources/webapp/xnat/java/org/nrg/xnat/restlet/util/UpdateExpirationCookie.java
+++ plugin-resources/webapp/xnat/java/org/nrg/xnat/restlet/util/UpdateExpirationCookie.java
@@ -16,15 +16,62 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletRequestWrapper;
 import javax.servlet.http.HttpServletResponse;
 
+import jj2000.j2k.util.StringFormatException;
+
 public class UpdateExpirationCookie implements Filter {
 	public static String name = "SESSION_EXPIRATION_TIME";
-	public static Cookie makeCookie (int sessionIdleTime) {
-		long start = System.currentTimeMillis();
-		Long expirationTimeMillis = Long.valueOf(start + (sessionIdleTime * 1000));
-		Cookie c = new Cookie(UpdateExpirationCookie.name, expirationTimeMillis.toString()); 
-		c.setPath("/");
-		return c;
+
+	static class CookieTuple {
+		final boolean flag;
+		final int maxIdleTime;
+		CookieTuple(Cookie c, int maxIdleTime) throws NumberFormatException, StringFormatException {
+			this.flag = parseFlag(c);
+			this.maxIdleTime = maxIdleTime;
+		}
+		boolean parseFlag(Cookie c) throws StringFormatException {
+			String[] values = c.getValue().split(",");
+			int numberFlag = Integer.parseInt(values[0]);
+			if (numberFlag == 0) {
+				return false;
+			}
+			else if (numberFlag == 1) {
+				return true;
+			}
+			else {
+				throw new StringFormatException("Expecting the first element of the tuple to be 0 or 1");
+			}
+		}
+		
+		static String createCookieTuple (boolean flag, int maxIdleTime) {
+			String flagString = flag ? "1" : "0";
+			return (flagString + "," + Integer.toString(maxIdleTime * 1000));
+ 		}
+		
+		Cookie makeCookie () {
+			Cookie c = new Cookie(UpdateExpirationCookie.name, CookieTuple.createCookieTuple(!this.flag, this.maxIdleTime));
+			c.setPath("/");
+			return c;
+		}
+		
+		static Cookie defaultCookie (int maxIdleTime) {
+			Cookie c = new Cookie(UpdateExpirationCookie.name, CookieTuple.createCookieTuple(false, maxIdleTime));
+			c.setPath("/");
+			return c;
+		} 
 	}
+	
+	private Cookie getRequestExpirationCookie (HttpServletRequest hq) {
+		Cookie[] requestCookies = hq.getCookies();
+		Cookie ret = null;
+		for (int i = 0; i < requestCookies.length; i++) {
+			Cookie c = requestCookies[i];
+			if (c.getName().equals(UpdateExpirationCookie.name)) {
+				ret = c;
+			}
+		}
+		return ret;
+	}
+	
 	@Override
 	public void destroy() {}
 	@Override
@@ -33,11 +80,26 @@ public class UpdateExpirationCookie implements Filter {
 						   FilterChain chain) throws IOException, ServletException {
 		HttpServletRequest hq = (HttpServletRequest) req;
 		HttpServletResponse hr = (HttpServletResponse) resp;
+		Cookie oldCookie = getRequestExpirationCookie(hq);
 		int sessionIdleTime = hq.getSession().getMaxInactiveInterval();
-		Cookie cookie = UpdateExpirationCookie.makeCookie(sessionIdleTime);
-		hr.addCookie(cookie);
+		CookieTuple cp = null;
+		Cookie c = null;
+		if (oldCookie == null) {
+			c = CookieTuple.defaultCookie(sessionIdleTime);
+		}
+		else {
+			try {
+				cp = new CookieTuple(oldCookie, sessionIdleTime);
+				c = cp.makeCookie();
+			}
+			catch (Exception e) {
+				c = CookieTuple.defaultCookie(sessionIdleTime);
+			}
+		}
+		hr.addCookie(c);
 		chain.doFilter(req,resp);
 	}
+	
 	@Override
 	public void init(FilterConfig fg) throws ServletException {}
 }

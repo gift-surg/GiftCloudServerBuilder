a1f5fbb5ff7e55f9034d2ef816c0f93b3f0b64ee
Kevin A. Archie <karchie@wustl.edu>
2012-05-25 14:02:45 -0500

    added unit tests for Siemens shadow header extraction
    Former-commit-id: 54d78782c832d77211f16ca821c61b003f0660db

---
+++ plugin-resources/webapp/xnat/javatest/org/nrg/xnat/dicom/DicomDataUsingTestCase.java
@@ -0,0 +1,102 @@
+/**
+ * Copyright (c) 2012 Washington University
+ */
+package org.nrg.xnat.dicom;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.URL;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+import junit.framework.TestCase;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.log4j.Logger;
+import org.nrg.io.FileWalkIterator;
+
+import com.google.common.collect.Lists;
+
+/**
+ * @author Kevin A. Archie <karchie@wustl.edu>
+ *
+ */
+public class DicomDataUsingTestCase extends TestCase implements Iterable<File> {
+    private static final String SAMPLE_1_URL = "http://nrg.wustl.edu/projects/DICOM/sample1.zip";
+    private static final File sample1;
+    static {
+        final String altPath = System.getProperty("dicom.sample.dir");
+        if (null == altPath) {
+            sample1 = new File(System.getProperty("java.io.tmpdir"), "dicom-sample1");
+        } else {
+            sample1 = new File(altPath);
+        }
+    }
+
+    public Iterator<File> iterator() {
+        return new FileWalkIterator(sample1, null);
+    }
+    
+    protected File getSampleData() { return sample1; }
+
+    protected DicomDataUsingTestCase() {
+        final Logger logger = Logger.getLogger(DicomDataUsingTestCase.class);
+        synchronized (DicomDataUsingTestCase.class) {
+            if (sample1.isDirectory()) {
+                // assume it's already filled.
+            } else {
+                try {
+                    final URL url = new URL(SAMPLE_1_URL);
+                    IOException ioe = null;
+                    final ZipInputStream zin = new ZipInputStream(url.openStream());
+                    try {
+                        for (ZipEntry ze; null != (ze = zin.getNextEntry()); ) {
+                            if (!ze.isDirectory()) {
+                                final File f = new File(sample1, ze.getName());
+                                f.getParentFile().mkdirs();
+                                final OutputStream out = new BufferedOutputStream(new FileOutputStream(f));
+                                try {
+                                    IOUtils.copy(zin, out);
+                                } catch (IOException e) {
+                                    throw ioe = e;
+                                } finally {
+                                    try {
+                                        out.close();
+                                    } catch (IOException e) {
+                                        if (null != ioe) {
+                                            logger.error("Unable to close sample data file", e);
+                                            throw ioe;
+                                        } else {
+                                            throw e;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    } catch (IOException e) {
+                        throw null == ioe ? ioe = e : ioe;
+                    } finally {
+                        try {
+                            zin.close();
+                        } catch (IOException e) {
+                            if (null != ioe) {
+                                logger.error("Unable to close zip entry", e);
+                                throw ioe;
+                            } else {
+                                throw e;
+                            }
+                        }
+                    }
+                } catch (IOException e) {
+                    throw new RuntimeException("Unable to download required sample DICOM data from " + SAMPLE_1_URL, e);
+                }
+            }
+        }
+    }
+}
---
+++ plugin-resources/webapp/xnat/javatest/org/nrg/xnat/dicom/SiemensShadowHeaderTest.java
@@ -0,0 +1,94 @@
+/**
+ * Copyright (c) 2012 Washington University
+ */
+package org.nrg.xnat.dicom;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Set;
+
+import org.dcm4che2.data.DicomObject;
+import org.dcm4che2.data.Tag;
+import org.junit.Test;
+import org.nrg.dcm.DicomUtils;
+import org.nrg.xft.XFTTable;
+import org.nrg.xnat.helpers.dicom.SiemensShadowHeader;
+
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Sets;
+
+/**
+ * @author Kevin A. Archie <karchie@wustl.edu>
+ *
+ */
+public final class SiemensShadowHeaderTest extends DicomDataUsingTestCase {
+    private static final String[] columns = {
+        "tag",  // tag name, never empty.
+        "subtag",  // for normal, non-sequence DICOM tags this is the empty string.
+        "vr",   // DICOM Value Representation  
+        "value", // Contents of the tag
+        "description"   // Description of the tag
+    };
+
+    private DicomObject getOneObject() {
+        for (final File f : this) {
+            DicomObject o;
+            try {
+                o = DicomUtils.read(f, Tag.PixelData);
+                if (null != o) {
+                    return o;
+                }
+            } catch (IOException skip) {}
+        }
+        return null;
+    }
+
+    @Test
+    public void testImageInstanceShadowHeader() throws IOException {
+        XFTTable table = new XFTTable();
+        table.initTable(columns);
+        table.resetRowCursor();
+        assertFalse(table.hasMoreRows());
+
+        final DicomObject o = getOneObject();
+        if (null == o) {
+            fail("Could not find a valid DICOM test object");
+        }
+
+        SiemensShadowHeader.addRows(table, o, o.get(0x00291010), Collections.<String>emptySet());
+        table.resetRowCursor();
+        assertTrue(table.hasMoreRows());
+        assertEquals(80, table.getNumRows());
+        final Set<String> fields = Sets.newHashSet();
+        for (int i = 0; table.hasMoreRows(); i++) {
+            final Object[] row = table.nextRow();
+            assertEquals("(0029,1010)", row[0]);
+            fields.add((String)row[1]);
+        }
+        assertEquals(table.getNumRows(), fields.size());
+    }
+
+    @Test
+    public void testImageInstanceShadowHeaderSelectedFields() throws IOException {
+        XFTTable table = new XFTTable();
+        table.initTable(columns);
+        table.resetRowCursor();
+        assertFalse(table.hasMoreRows());
+        
+        final DicomObject o = getOneObject();
+        if (null == o) {
+            fail("Could not find a valid DICOM test object");
+        }
+
+        final Set<String> fields = ImmutableSet.of("SlicePosition_PCS", "MultistepIndex");
+        
+        SiemensShadowHeader.addRows(table, o, o.get(0x00291010), fields);
+        table.resetRowCursor();
+        assertTrue(table.hasMoreRows());
+        assertEquals(fields.size(), table.getNumRows());
+        assertTrue(fields.contains(table.nextRow()[1]));
+        assertTrue(fields.contains(table.nextRow()[1]));
+        assertFalse(table.hasMoreRows());
+    }
+}

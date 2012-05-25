61c768f06eb126bd8262d995ff5025342c3c8518
Rick Herrick <rick.herrick@wustl.edu>
2012-05-25 13:54:06 -0500

    Fix for report a problem page.
    Former-commit-id: 63c579b2cc5518cf5985a89b0885ee596bb851dc

--- plugin-resources/webapp/xnat/java/org/nrg/xnat/restlet/services/SettingsRestlet.java
+++ plugin-resources/webapp/xnat/java/org/nrg/xnat/restlet/services/SettingsRestlet.java
@@ -8,7 +8,18 @@ import org.codehaus.jackson.map.JsonMappingException;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.nrg.framework.exceptions.NrgServiceError;
 import org.nrg.framework.exceptions.NrgServiceRuntimeException;
+import org.nrg.mail.api.NotificationType;
+import org.nrg.notify.api.CategoryScope;
+import org.nrg.notify.entities.Category;
+import org.nrg.notify.entities.Definition;
+import org.nrg.notify.entities.Subscription;
+import org.nrg.notify.services.NotificationService;
+import org.nrg.xdat.XDAT;
+import org.nrg.xdat.model.ArcArchivespecificationNotificationTypeI;
 import org.nrg.xdat.om.ArcArchivespecification;
+import org.nrg.xdat.om.ArcArchivespecificationNotificationType;
+import org.nrg.xdat.om.XdatUser;
+import org.nrg.xdat.security.XDATUser;
 import org.nrg.xdat.turbine.utils.PopulateItem;
 import org.nrg.xft.XFT;
 import org.nrg.xft.XFTItem;
@@ -35,6 +46,7 @@ import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class SettingsRestlet extends SecureResource {
@@ -143,10 +155,12 @@ public class SettingsRestlet extends SecureResource {
         try {
             if (!StringUtils.isBlank(_property) && !_property.equals("initialize")) {
                 setDiscreteProperty();
+                checkNotifications(_arcSpec);
             } else
                 // We will only enter this if _property is "initialize", so that means we need to set up the arc spec entry.
                 if (!StringUtils.isBlank(_property)) {
                     initializeArcSpec();
+                    checkNotifications(_arcSpec);
                 } else {
                     setPropertiesFromFormData();
             }
@@ -332,7 +346,120 @@ public class SettingsRestlet extends SecureResource {
             }
         }
     }
+    
+    /**
+     * Checks whether site-side notifications exist and initializes them if not.
+     *
+     * @param arcSpec The archive specification object.
+     * @throws Exception
+     */
+    private void checkNotifications(ArcArchivespecification arcSpec) throws Exception {
+        // Check whether any notification types already exist and clear them if so.
+        clearArcSpecNotifications(arcSpec);
 
+        for (NotificationType type : NotificationType.values()) {
+            Definition definition = retrieveSiteEventDefinition(type);
+            List<Subscription> subscriptions = getNotificationService().getSubscriptionService().getSubscriptionsForDefinition(definition);
+
+            ArcArchivespecificationNotificationTypeI typeObj = new ArcArchivespecificationNotificationType();
+            typeObj.setNotificationType(type.id());
+            if (subscriptions == null || subscriptions.size() == 0) {
+                typeObj.setEmailAddresses(getSiteAdminAccount(arcSpec) + " <" + arcSpec.getSiteAdminEmail() + ">");
+            } else {
+                StringBuilder buffer = new StringBuilder();
+                boolean isFirst = true;
+                for (Subscription subscription : subscriptions) {
+                    if (isFirst) {
+                        isFirst = false;
+                    } else {
+                        buffer.append(", ");
+}
+                    buffer.append(subscription.getSubscriber());
+                }
+                typeObj.setEmailAddresses(buffer.toString());
+            }
+            arcSpec.addNotificationTypes_notificationType(typeObj);
+        }
+    }
+
+    /**
+     * @param arcSpec
+     */
+    private void clearArcSpecNotifications(ArcArchivespecification arcSpec) {
+        List<ArcArchivespecificationNotificationTypeI> notificationTypes;
+        while ((notificationTypes = arcSpec.getNotificationTypes_notificationType()) != null && notificationTypes.size() > 0) {
+            arcSpec.removeNotificationTypes_notificationType(0);
+        }
+    }
+
+    /**
+     * @param type The type for which the definition and its associated category should be created or retrieved.
+     * @return The existing or newly created definition.
+     */
+    private Definition retrieveSiteEventDefinition(NotificationType type) {
+        Category category = getNotificationService().getCategoryService().getCategoryByScopeAndEvent(CategoryScope.Site, type.toString());
+        if (category == null) {
+            category = initializeSiteEventCategory(type.toString());
+        }
+        List<Definition> definitions = getNotificationService().getDefinitionService().getDefinitionsForCategory(category);
+        Definition definition;
+        if (definitions == null || definitions.size() == 0) {
+            definition = initializeSiteEventDefinition(category);
+        } else {
+            definition = definitions.get(0);
+        }
+        return definition;
+    }
+
+    /**
+     * Gets the notification service instance.
+     *
+     * @return The notification service instance.
+     */
+    private NotificationService getNotificationService() {
+        if (_notificationService == null) {
+            _notificationService = XDAT.getNotificationService();
+        }
+        return _notificationService;
+    }
+
+    /**
+     * @param event The event for which a category should be created.
+     * @return The newly created category object.
+     */
+    private Category initializeSiteEventCategory(String event) {
+        Category category = getNotificationService().getCategoryService().newEntity();
+        category.setScope(CategoryScope.Site);
+        category.setEvent(event);
+        getNotificationService().getCategoryService().create(category);
+        return category;
+    }
+
+
+    /**
+     * @param category The category for which a definition should be created.
+     * @return The newly created definition object.
+     */
+    private Definition initializeSiteEventDefinition(Category category) {
+        Definition definition = getNotificationService().getDefinitionService().newEntity();
+        definition.setCategory(category);
+        getNotificationService().getDefinitionService().create(definition);
+        return definition;
+    }
+
+    /**
+     * @param arcSpec
+     * @return
+     */
+    private String getSiteAdminAccount(ArcArchivespecification arcSpec) {
+        List<XdatUser> users = XDATUser.getXdatUsersByField("xdat:user/email", arcSpec.getSiteAdminEmail(), null, true);
+        if (users == null || users.size() == 0) {
+            throw new RuntimeException("Can't find anything for the site admin email account! It must be associated with a user account: " + arcSpec.getSiteAdminEmail());
+        }
+        return users.get(0).getLogin();
+    }
+
+    private NotificationService _notificationService;
     private static final Log _log = LogFactory.getLog(SettingsRestlet.class);
     private ArcArchivespecification _arcSpec;
     private String _property;
--- plugin-resources/webapp/xnat/java/org/nrg/xnat/turbine/modules/actions/ReportIssue.java
+++ plugin-resources/webapp/xnat/java/org/nrg/xnat/turbine/modules/actions/ReportIssue.java
@@ -1,8 +1,8 @@
 //Copyright 2007 Washington University School of Medicine All Rights Reserved
 /*
- * Created on Aug 7, 2007
- *
- */
+* Created on Aug 7, 2007
+*
+*/
 package org.nrg.xnat.turbine.modules.actions;
 
 import org.apache.commons.fileupload.FileItem;
@@ -13,7 +13,18 @@ import org.apache.turbine.util.parser.ParameterParser;
 import org.apache.velocity.app.Velocity;
 import org.apache.velocity.context.Context;
 import org.nrg.mail.api.MailMessage;
+import org.nrg.mail.api.NotificationType;
+import org.nrg.notify.api.CategoryScope;
+import org.nrg.notify.api.SubscriberType;
+import org.nrg.notify.entities.Category;
+import org.nrg.notify.entities.Channel;
+import org.nrg.notify.entities.Definition;
+import org.nrg.notify.entities.Subscriber;
+import org.nrg.notify.entities.Subscription;
+import org.nrg.notify.exceptions.DuplicateSubscriberException;
+import org.nrg.notify.services.NotificationService;
 import org.nrg.xdat.XDAT;
+import org.nrg.xdat.om.ArcArchivespecification;
 import org.nrg.xdat.security.XDATUser;
 import org.nrg.xdat.turbine.modules.actions.SecureAction;
 import org.nrg.xdat.turbine.utils.AccessLogger;
@@ -25,34 +36,38 @@ import org.nrg.xnat.utils.FileUtils;
 
 import java.io.File;
 import java.io.StringWriter;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class ReportIssue extends SecureAction {
-	private static final Logger logger = Logger.getLogger(ReportIssue.class);
+                private static final Logger logger = Logger.getLogger(ReportIssue.class);
 
-	private static final String HTTP_USER_AGENT = "User-Agent";
-	private static final String HTTP_HOST = "Host";
-	private static final String JAVA_VENDOR = "java.vendor";
-	private static final String JAVA_VERSION = "java.version";
-	private static final String JAVA_OS_VERSION = "os.version";
-	private static final String JAVA_OS_ARCH = "os.arch";
-	private static final String JAVA_OS_NAME = "os.name";
+                private static final String HTTP_USER_AGENT = "User-Agent";
+                private static final String HTTP_HOST = "Host";
+                private static final String JAVA_VENDOR = "java.vendor";
+                private static final String JAVA_VERSION = "java.version";
+                private static final String JAVA_OS_VERSION = "os.version";
+                private static final String JAVA_OS_ARCH = "os.arch";
+                private static final String JAVA_OS_NAME = "os.name";
+                public static final String ADMIN_USERNAME_FOR_SUBSCRIPTION = "ADMIN_USER";
+                
+                @Override
+                public void doPerform(RunData data, Context context) throws Exception {
 
-	@Override
-	public void doPerform(RunData data, Context context) throws Exception {
+                                final String adminEmail = XFT.GetAdminEmail();
+                                
+                                final XDATUser user = TurbineUtils.getUser(data);
+                                final ParameterParser parameters = data.getParameters();
+                                final String body = emailBody(user, parameters, data, context);
 
-		final String adminEmail = XFT.GetAdminEmail();
-		final XDATUser user = TurbineUtils.getUser(data);
-		final ParameterParser parameters = data.getParameters();
-		final String body = emailBody(user, parameters, data, context);
-
-		String subject = TurbineUtils.GetSystemName() + " Issue Report from " + user.getLogin();
+                                String subject = TurbineUtils.GetSystemName() + " Issue Report from " + user.getLogin();
         // TODO: Need to figure out how to handle attachments in notifications.
-		Map<String, File> attachments = getAttachmentMap(data.getSession().getId(), parameters);
+                                Map<String, File> attachments = getAttachmentMap(data.getSession().getId(), parameters);
 
-		// XDAT.getMailService().sendHtmlMessage(adminEmail, new String[] { adminEmail }, null, null, subject, body, null, attachments);
+                                // XDAT.getMailService().sendHtmlMessage(adminEmail, new String[] { adminEmail }, null, null, subject, body, null, attachments);
         Map<String, Object> properties = new HashMap<String, Object>();
         properties.put(MailMessage.PROP_FROM, adminEmail);
         properties.put(MailMessage.PROP_SUBJECT, subject);
@@ -60,65 +75,133 @@ public class ReportIssue extends SecureAction {
         if (attachments != null && attachments.size() > 0) {
             properties.put(MailMessage.PROP_ATTACHMENTS, attachments);
         }
+        checkNotifications("Issue", adminEmail);
         XDAT.getNotificationService().createNotification("Issue", properties);
-	}
+                }
+                
+    private void checkNotifications(String event, String adminEmail) throws Exception {
+                Channel channel = getHtmlMailChannel();
+                Category category = getNotificationService().getCategoryService().getCategoryByScopeAndEvent(CategoryScope.Site, event);
+        if (category == null) {
+            category = getNotificationService().getCategoryService().newEntity();
+            category.setScope(CategoryScope.Site);
+            category.setEvent(event);
+            getNotificationService().getCategoryService().create(category);
+        }
+        Definition definition;
+        List<Definition> definitions = getNotificationService().getDefinitionService().getDefinitionsForCategory(category);
+        if (definitions == null || definitions.size() == 0) {
+            definition = getNotificationService().getDefinitionService().newEntity();
+            definition.setCategory(category);
+            getNotificationService().getDefinitionService().create(definition);
+        } else {
+            definition = definitions.get(0);
+        }
+                
+        Subscriber subscriber = getNotificationService().getSubscriberService().getSubscriberByName( ADMIN_USERNAME_FOR_SUBSCRIPTION);
+        if (subscriber == null) {
+            try {
+                subscriber = getNotificationService().getSubscriberService().createSubscriber(ADMIN_USERNAME_FOR_SUBSCRIPTION, adminEmail);
+            } catch (DuplicateSubscriberException exception) {
+                // This shouldn't happen, since we just checked for the subscriber's existence.
+            }
+        }
+        
+        List<Subscription> subs = getNotificationService().getSubscriptionService().getSubscriptionsForDefinition(definition);
+        boolean found = false;
+        for(Subscription sub : subs){
+        	List<Long> ids = new ArrayList<Long>();
+        	for(Channel ch : sub.getChannels()){
+        		ids.add(ch.getId());
+        	}
+        	if(sub.getSubscriber().getId()==(subscriber.getId()) && sub.getSubscriberType().equals(SubscriberType.User) && ids.contains(channel.getId())){
+            	found = true;
+            	break;
+            }
+        }
+        if (!found){
+        	getNotificationService().subscribe(subscriber, SubscriberType.User, definition, channel);
+        }
+        
+    }
 
-	private Map<String, File> getAttachmentMap(String sessionId, ParameterParser parameters) {
-		Map<String, File> attachmentMap = new HashMap<String, File>();
-		
-		final FileItem fi = parameters.getFileItem("upload");
-		if (fi != null) {
-			final String cachePath = location(ArcSpecManager.GetInstance().getGlobalCachePath(), "issuereports", sessionId);
-			checkFolder(cachePath);
+    private Channel getHtmlMailChannel() {
+        Channel channel = getNotificationService().getChannelService().getChannel("htmlMail");
+        if (channel == null) {
+            channel = getNotificationService().getChannelService().newEntity();
+            channel.setName("htmlMail");
+            channel.setFormat("text/html");
+            getNotificationService().getChannelService().create(channel);
+}
+        return channel;
+    }
+    
+    private NotificationService _notificationService;
+    private NotificationService getNotificationService() {
+        if (_notificationService == null) {
+            _notificationService = XDAT.getNotificationService();
+        }
+        return _notificationService;
+    }
+    
+    
+    
+                private Map<String, File> getAttachmentMap(String sessionId, ParameterParser parameters) {
+                                Map<String, File> attachmentMap = new HashMap<String, File>();
+                                
+                                final FileItem fi = parameters.getFileItem("upload");
+                                if (fi != null) {
+                                                final String cachePath = location(ArcSpecManager.GetInstance().getGlobalCachePath(), "issuereports", sessionId);
+                                                checkFolder(cachePath);
 
-			final File file = new File(location(cachePath, fi.getName()));
-			try {
-				fi.write(file);
-				attachmentMap.put(fi.getName(), file);
-			} catch (Exception exception) {
-				logger.warn("Could not attach file, " + file.getAbsolutePath(), exception);
-			}
-		}
+                                                final File file = new File(location(cachePath, fi.getName()));
+                                                try {
+                                                                fi.write(file);
+                                                                attachmentMap.put(fi.getName(), file);
+                                                } catch (Exception exception) {
+                                                                logger.warn("Could not attach file, " + file.getAbsolutePath(), exception);
+                                                }
+                                }
 
-		return attachmentMap;
-	}
+                                return attachmentMap;
+                }
 
-	private String emailBody(XDATUser user, ParameterParser parameters, RunData data, Context context) throws Exception {
-		context.put("summary", parameters.get("summary"));
-		context.put("description", parameters.get("description"));
+                private String emailBody(XDATUser user, ParameterParser parameters, RunData data, Context context) throws Exception {
+                                context.put("summary", parameters.get("summary"));
+                                context.put("description", parameters.get("description"));
 
-		context.put("time", (new Date()).toString());
+                                context.put("time", (new Date()).toString());
 
-		context.put("user_agent", data.getRequest().getHeader(HTTP_USER_AGENT));
-		context.put("xnat_host", data.getRequest().getHeader(HTTP_HOST));
-		context.put("remote_addr", AccessLogger.GetRequestIp(data.getRequest()));
-		context.put("server_info", data.getServletContext().getServerInfo());
+                                context.put("user_agent", data.getRequest().getHeader(HTTP_USER_AGENT));
+                                context.put("xnat_host", data.getRequest().getHeader(HTTP_HOST));
+                                context.put("remote_addr", AccessLogger.GetRequestIp(data.getRequest()));
+                                context.put("server_info", data.getServletContext().getServerInfo());
 
-		context.put("os_name", System.getProperty(JAVA_OS_NAME));
-		context.put("os_arch", System.getProperty(JAVA_OS_ARCH));
-		context.put("os_version", System.getProperty(JAVA_OS_VERSION));
-		context.put("java_version", System.getProperty(JAVA_VERSION));
-		context.put("java_vendor", System.getProperty(JAVA_VENDOR));
+                                context.put("os_name", System.getProperty(JAVA_OS_NAME));
+                                context.put("os_arch", System.getProperty(JAVA_OS_ARCH));
+                                context.put("os_version", System.getProperty(JAVA_OS_VERSION));
+                                context.put("java_version", System.getProperty(JAVA_VERSION));
+                                context.put("java_vendor", System.getProperty(JAVA_VENDOR));
 
-		context.put("xnat_version", FileUtils.getXNATVersion());
+                                context.put("xnat_version", FileUtils.getXNATVersion());
 
-		context.put("user", user);
-		context.put("postgres_version", (String) PoolDBUtils.ReturnStatisticQuery("SELECT version();", "version", user.getDBName(), user.getLogin()));
+                                context.put("user", user);
+                                context.put("postgres_version", (String) PoolDBUtils.ReturnStatisticQuery("SELECT version();", "version", user.getDBName(), user.getLogin()));
 
-		final StringWriter sw = new StringWriter();
-		Velocity.getTemplate("/screens/email/issue_report.vm").merge(context, sw);
-		return sw.toString();
-	}
+                                final StringWriter sw = new StringWriter();
+                                Velocity.getTemplate("/screens/email/issue_report.vm").merge(context, sw);
+                                return sw.toString();
+                }
 
-	private void checkFolder(String path) {
-		final File f = new File(path);
-		if (!f.exists()) {
-			f.mkdirs();
-		}
-	}
+                private void checkFolder(String path) {
+                                final File f = new File(path);
+                                if (!f.exists()) {
+                                                f.mkdirs();
+                                }
+                }
 
-	private String location(String... pathParts) {
-		return StringUtils.join(pathParts, File.separator);
-	}
+                private String location(String... pathParts) {
+                                return StringUtils.join(pathParts, File.separator);
+                }
 
 }

2ae9c229d47d76d78901013577f727a4f00f7f52
Kevin A. Archie <karchie@wustl.edu>
2012-06-19 16:25:38 -0500

    fixes for anonymization XNAT-1340, XNAT-1343
    Former-commit-id: 510e3ac34efcb3301a993992892946fd47a6e357

--- plugin-resources/webapp/xnat/java/org/nrg/xnat/turbine/modules/actions/DeleteXdatElementSecurity.java
+++ plugin-resources/webapp/xnat/java/org/nrg/xnat/turbine/modules/actions/DeleteXdatElementSecurity.java
@@ -17,8 +17,6 @@ import org.nrg.xft.db.PoolDBUtils;
 import java.sql.SQLException;
 
 public class DeleteXdatElementSecurity extends DeleteAction {
-
-    @Override
     protected void postDelete(RunData data, Context context) {
         final String dataType = TurbineUtils.GetPassedParameter("search_value", data).toString();
         final String query1 = "DELETE FROM xdat_element_access WHERE xdat_element_access_id IN ( select xdat_element_access_id from xdat_element_access xea LEFT JOIN xdat_element_security xes ON xea.element_name=xes.element_name WHERE xes.element_name IS NULL);";

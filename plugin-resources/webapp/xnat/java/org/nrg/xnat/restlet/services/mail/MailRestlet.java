bd7097f1fa2be0d594789d28c5f693c993d4fca5
Rick Herrick <rick.herrick@wustl.edu>
2012-07-18 13:54:50 -0500

    Fixed bad error message formatting.
    Former-commit-id: fe8adc230bf5a84d5a5a31a8e0dab1953df3e02d

--- plugin-resources/webapp/xnat/java/org/nrg/xnat/restlet/services/mail/MailRestlet.java
+++ plugin-resources/webapp/xnat/java/org/nrg/xnat/restlet/services/mail/MailRestlet.java
@@ -37,34 +37,34 @@ import java.util.Map;
  * @author Rick Herrick <rick.herrick@wustl.edu>
  */
 public class MailRestlet extends SecureResource {
-	public static final String PARAM_BCC = "bcc";
-	public static final String PARAM_CC = "cc";
-	public static final String PARAM_TO = "to";
-	public static final String PARAM_HTML = "html";
-	public static final String PARAM_SUBJECT = "subject";
-	public static final String PARAM_TEXT = "text";
+    public static final String PARAM_BCC = "bcc";
+    public static final String PARAM_CC = "cc";
+    public static final String PARAM_TO = "to";
+    public static final String PARAM_HTML = "html";
+    public static final String PARAM_SUBJECT = "subject";
+    public static final String PARAM_TEXT = "text";
 
-	public MailRestlet(Context context, Request request, Response response) {
-		super(context, request, response);
-		this.getVariants().add(new Variant(MediaType.ALL));
-		_log.debug("Instantiated MailRestlet instance");
-	}
+    public MailRestlet(Context context, Request request, Response response) {
+        super(context, request, response);
+        this.getVariants().add(new Variant(MediaType.ALL));
+        _log.debug("Instantiated MailRestlet instance");
+    }
 
-	@Override
-	public boolean allowGet(){
-		return false;
-	}
+    @Override
+    public boolean allowGet(){
+        return false;
+    }
 
-	@Override
-	public boolean allowPost() {
-		return true;
-	}
+    @Override
+    public boolean allowPost() {
+        return true;
+    }
 
-	@Override
-	public void handlePost() {
-		// Process incoming parameters
-		try {
-			Representation entity = this.getRequest().getEntity();
+    @Override
+    public void handlePost() {
+        // Process incoming parameters
+        try {
+            Representation entity = this.getRequest().getEntity();
 
             if (!extractParameters(entity)) {
                 _log.warn("I was unable to properly parse an incoming request, check the logs for more information, returning: " + Status.CLIENT_ERROR_BAD_REQUEST);
@@ -79,27 +79,27 @@ public class MailRestlet extends SecureResource {
             message.setOnBehalfOf(user.getEmail());
 
             // Handle all the addresses.
-			String[] tos = getAddresses(PARAM_TO);
-			if (tos == null || tos.length == 0) {
-				throw new ClientException("No valid user IDs were provided for the to address.");
-			}
+            String[] tos = getAddresses(PARAM_TO);
+            if (tos == null || tos.length == 0) {
+                throw new ClientException("No valid user IDs were provided for the to address.");
+            }
             message.setTos(tos);
-			String[] ccs = getAddresses(PARAM_CC);
+            String[] ccs = getAddresses(PARAM_CC);
             if (ccs == null || ccs.length == 0) {
                 message.setCcs(ccs);
             }
-			String[] bccs = getAddresses(PARAM_BCC);
+            String[] bccs = getAddresses(PARAM_BCC);
             if (bccs == null || bccs.length == 0) {
                 message.setBccs(bccs);
             }
 
             // Set the message guts.
-			message.setSubject(getSubject());
-			String text = getText();
+            message.setSubject(getSubject());
+            String text = getText();
             if (!StringUtils.isBlank(text)) {
                 message.setText(text);
             }
-			String html = getHtml();
+            String html = getHtml();
             if (!StringUtils.isBlank(html)) {
                 message.setHtml(html);
             }
@@ -114,24 +114,24 @@ public class MailRestlet extends SecureResource {
             }
 
             // Finally send the mail.
-			XDAT.getMailService().sendMessage(message);
-		} catch (ClientException exception) {
-			respondToException(exception, (exception.status != null) ? exception.status : Status.CLIENT_ERROR_BAD_REQUEST);
-		} catch (Exception exception) {
-			respondToException(exception, Status.SERVER_ERROR_INTERNAL);
-		}
+            XDAT.getMailService().sendMessage(message);
+        } catch (ClientException exception) {
+            respondToException(exception, (exception.status != null) ? exception.status : Status.CLIENT_ERROR_BAD_REQUEST);
+        } catch (Exception exception) {
+            respondToException(exception, Status.SERVER_ERROR_INTERNAL);
+        }
 
-		if (_issues.size() > 0) {
-			returnString("OK with warnings, see log.", Status.SUCCESS_OK);
-			StringBuilder issues = new StringBuilder("Found a number of issues sending email:\n\n");
-			for (String issue : _issues) {
-				issues.append(String.format(FORMAT_ISSUE, issue));
-			}
-			_log.warn(issues);
-		} else {
-			returnString("OK", Status.SUCCESS_OK);
-		}
-	}
+        if (_issues.size() > 0) {
+            returnString("OK with warnings, see log.", Status.SUCCESS_OK);
+            StringBuilder issues = new StringBuilder("Found a number of issues sending email:\n\n");
+            for (String issue : _issues) {
+                issues.append(String.format(FORMAT_ISSUE, issue));
+            }
+            _log.warn(issues);
+        } else {
+            returnString("OK", Status.SUCCESS_OK);
+        }
+    }
 
     private boolean extractParameters(Representation entity) throws Exception {
         MediaType mediaType = entity.getMediaType();
@@ -161,52 +161,52 @@ public class MailRestlet extends SecureResource {
     }
 
     @Override
-	public void handleParam(final String key, final Object value) throws ClientException{
-		if (!_parameters.containsKey(key)) {
-			if (_log.isDebugEnabled()) {
-				_log.debug(String.format("Creating new value list for parameter: %s", key));
-			}
-			_parameters.put(key, new ArrayList<String>());
-		}
+    public void handleParam(final String key, final Object value) throws ClientException{
+        if (!_parameters.containsKey(key)) {
+            if (_log.isDebugEnabled()) {
+                _log.debug(String.format("Creating new value list for parameter: %s", key));
+            }
+            _parameters.put(key, new ArrayList<String>());
+        }
 
-		if (_log.isDebugEnabled()) {
-			_log.debug(String.format("Adding value for parameter %s: [%s]", key, value));
-		}
+        if (_log.isDebugEnabled()) {
+            _log.debug(String.format("Adding value for parameter %s: [%s]", key, value));
+        }
 
-		// For now assume that value is a string until we can support other types.
-		_parameters.get(key).add((String) value);
-	}
+        // For now assume that value is a string until we can support other types.
+        _parameters.get(key).add((String) value);
+    }
 
-	private String[] getAddresses(String type) {
-		List<String> addresses = new ArrayList<String>();
-		if (_parameters.containsKey(type)) {
-			List<String> ids = _parameters.get(type);
-			for (String id : ids) {
-				try {
-					// Try to parse this as an integer. If it's a user ID, it'll parse and we'll use the get by ID function.
-					Integer.parseInt(id);
-					XdatUser requested = XDATUser.getXdatUsersByXdatUserId(id, user, true);
-					if (requested == null) {
-						addIssue("The user ID %s was not found in the system and was not included on the email.");
-					} else {
-						addresses.add(requested.getEmail());
-					}
-				} catch (NumberFormatException exception) {
-					// If not an integer, we'll try it as an email address. It has to match an existing email address in the system!
-					ArrayList<XdatUser> users = XDATUser.getXdatUsersByField("xdat:user/email", id, user, true);
-					if (users.size() == 0) {
-						addIssue("The user ID %s was not found in the system and was not included on the email.");
-					} else {
-						addresses.add(id);
-					}
-				}
-			}
-		}
-		return addresses.toArray(new String[addresses.size()]);
-	}
+    private String[] getAddresses(String type) {
+        List<String> addresses = new ArrayList<String>();
+        if (_parameters.containsKey(type)) {
+            List<String> ids = _parameters.get(type);
+            for (String id : ids) {
+                try {
+                    // Try to parse this as an integer. If it's a user ID, it'll parse and we'll use the get by ID function.
+                    Integer.parseInt(id);
+                    XdatUser requested = XDATUser.getXdatUsersByXdatUserId(id, user, true);
+                    if (requested == null) {
+                        addIssue(String.format("The user ID %s was not found in the system and was not included on the email.", id));
+                    } else {
+                        addresses.add(requested.getEmail());
+                    }
+                } catch (NumberFormatException exception) {
+                    // If not an integer, we'll try it as an email address. It has to match an existing email address in the system!
+                    ArrayList<XdatUser> users = XDATUser.getXdatUsersByField("xdat:user/email", id, user, true);
+                    if (users.size() == 0) {
+                        addIssue(String.format("The user email %s was not found in the system and was not included on the email.", id));
+                    } else {
+                        addresses.add(id);
+                    }
+                }
+            }
+        }
+        return addresses.toArray(new String[addresses.size()]);
+    }
 
-	private Map<String, List<String>> getHeaders() {
-		Map<String, List<String>> headers = new HashMap<String, List<String>>();
+    private Map<String, List<String>> getHeaders() {
+        Map<String, List<String>> headers = new HashMap<String, List<String>>();
         for (String key : _parameters.keySet()) {
             if (key.startsWith("header:")) {
                 List<String> value = _parameters.get(key);
@@ -214,71 +214,71 @@ public class MailRestlet extends SecureResource {
             }
         }
         return headers;
-	}
+    }
 
-	private void addIssue(String issue) {
-		_issues.add(issue);
-	}
+    private void addIssue(String issue) {
+        _issues.add(issue);
+    }
 
-	private String getSubject() {
-		return getParameter(PARAM_SUBJECT);
-	}
+    private String getSubject() {
+        return getParameter(PARAM_SUBJECT);
+    }
 
-	private String getText() {
-		return getParameter(PARAM_TEXT);
-	}
+    private String getText() {
+        return getParameter(PARAM_TEXT);
+    }
 
-	private String getHtml() {
-		return getParameter(PARAM_HTML);
-	}
+    private String getHtml() {
+        return getParameter(PARAM_HTML);
+    }
 
-	private String getParameter(String parameter) {
-       return _parameters.containsKey(parameter) ? _parameters.get(parameter).get(0) : null;
-	}
+    private String getParameter(String parameter) {
+        return _parameters.containsKey(parameter) ? _parameters.get(parameter).get(0) : null;
+    }
 
-	private boolean validateParameters() {
-		List<String> validationErrors = new ArrayList<String>();
+    private boolean validateParameters() {
+        List<String> validationErrors = new ArrayList<String>();
 
-		if (!_parameters.containsKey(PARAM_TO)) {
-			validationErrors.add("You must specify at least one recipient for your message.");
-		}
-		if (!_parameters.containsKey(PARAM_SUBJECT)) {
-			validationErrors.add("Emails sent through this service must have a subject.");
-		} else if (_parameters.get(PARAM_SUBJECT).size() > 1) {
-			validationErrors.add("You can only provide a single value for the email subject.");
-		}
-		if (!_parameters.containsKey(PARAM_TEXT) && !_parameters.containsKey(PARAM_HTML)) {
-			validationErrors.add("Emails sent through this service must have a message body in either the .");
-		} else {
-			if (_parameters.containsKey(PARAM_TEXT) && _parameters.get(PARAM_TEXT).size() > 1) {
-				validationErrors.add("You can only provide a single value for the email text body.");
-			}
-			if (_parameters.containsKey(PARAM_HTML) && _parameters.get(PARAM_HTML).size() > 1) {
-				validationErrors.add("You can only provide a single value for the email HTML body.");
-			}
-		}
+        if (!_parameters.containsKey(PARAM_TO)) {
+            validationErrors.add("You must specify at least one recipient for your message.");
+        }
+        if (!_parameters.containsKey(PARAM_SUBJECT)) {
+            validationErrors.add("Emails sent through this service must have a subject.");
+        } else if (_parameters.get(PARAM_SUBJECT).size() > 1) {
+            validationErrors.add("You can only provide a single value for the email subject.");
+        }
+        if (!_parameters.containsKey(PARAM_TEXT) && !_parameters.containsKey(PARAM_HTML)) {
+            validationErrors.add("Emails sent through this service must have a message body in either the .");
+        } else {
+            if (_parameters.containsKey(PARAM_TEXT) && _parameters.get(PARAM_TEXT).size() > 1) {
+                validationErrors.add("You can only provide a single value for the email text body.");
+            }
+            if (_parameters.containsKey(PARAM_HTML) && _parameters.get(PARAM_HTML).size() > 1) {
+                validationErrors.add("You can only provide a single value for the email HTML body.");
+            }
+        }
 
-		if (validationErrors.size() > 0) {
-			String message = formatErrorMessage(validationErrors);
-			getResponse().setStatus(Status.CLIENT_ERROR_BAD_REQUEST, message);
-			return false;
-		}
+        if (validationErrors.size() > 0) {
+            String message = formatErrorMessage(validationErrors);
+            getResponse().setStatus(Status.CLIENT_ERROR_BAD_REQUEST, message);
+            return false;
+        }
 
-		return true;
-	}
+        return true;
+    }
 
-	private String formatErrorMessage(List<String> errors) {
-		StringBuilder message = new StringBuilder("The following problems were found with your submission:\n\n");
-		for (String error : errors) {
-			message.append(String.format(FORMAT_ISSUE, error));
-		}
-		return message.toString();
-	}
+    private String formatErrorMessage(List<String> errors) {
+        StringBuilder message = new StringBuilder("The following problems were found with your submission:\n\n");
+        for (String error : errors) {
+            message.append(String.format(FORMAT_ISSUE, error));
+        }
+        return message.toString();
+    }
 
-private static final String FORMAT_ISSUE = " * %s\n";
-	private static final Log _log = LogFactory.getLog(MailRestlet.class);
+    private static final String FORMAT_ISSUE = " * %s\n";
+    private static final Log _log = LogFactory.getLog(MailRestlet.class);
 
-	private Map<String, List<String>> _parameters = new HashMap<String, List<String>>();
-	private List<String> _issues = new ArrayList<String>();
-	private Map<String, File> _files = new HashMap<String, File>();
+    private Map<String, List<String>> _parameters = new HashMap<String, List<String>>();
+    private List<String> _issues = new ArrayList<String>();
+    private Map<String, File> _files = new HashMap<String, File>();
 }

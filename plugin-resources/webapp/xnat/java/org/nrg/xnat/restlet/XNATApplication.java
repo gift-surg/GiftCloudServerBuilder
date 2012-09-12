f8d12b5b3d8870736b74d195e8eaa3adbb31d2b8
Ryan Fujiwara <rpfujiw@gmail.com>
2012-09-12 13:52:14 -0500

    XNAT-1549 Create Rest Call to return active sessions for specified user
    Former-commit-id: 5a80b5fdfa08a7c9c27035c4b7290c6da45fff51

--- plugin-resources/webapp/xnat/java/org/nrg/xnat/restlet/XNATApplication.java
+++ plugin-resources/webapp/xnat/java/org/nrg/xnat/restlet/XNATApplication.java
@@ -11,6 +11,7 @@ import org.nrg.framework.utilities.Reflection;
 import org.nrg.xdat.XDAT;
 import org.nrg.xnat.helpers.dicom.DicomDump;
 import org.nrg.xnat.helpers.editscript.DicomEdit;
+import org.nrg.xnat.restlet.actions.UserSessionId;
 import org.nrg.xnat.restlet.guard.XnatSecureGuard;
 import org.nrg.xnat.restlet.resources.*;
 import org.nrg.xnat.restlet.resources.files.CatalogResource;
@@ -239,7 +240,8 @@ public class XNATApplication extends Application {
         attachArchiveURI(router,"/projects/{PROJECT_ID}/experiments/{EXPT_ID}/DIR",DIRResource.class);
         attachArchiveURI(router,"/experiments/{EXPT_ID}/XAR",DIRResource.class);
         attachArchiveURI(router,"/projects/{PROJECT_ID}/experiments/{EXPT_ID}/XAR",DIRResource.class);
-
+        attachArchiveURI(router,"/user/{USER_ID}/sessions",UserSessionId.class);//GET returns number of user sessions
+        
         // Users Cache Space
         router.attach("/user/cache/resources",UserCacheResource.class);
         router.attach("/user/cache/resources/{XNAME}",UserCacheResource.class);

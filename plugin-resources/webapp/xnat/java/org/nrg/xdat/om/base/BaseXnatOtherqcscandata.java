1d192dc8446150feff1d944a2c43b8a3079ddd75
Tim Olsen <tim@radiologics.com>
2012-09-26 16:51:16 -0500

    Add catch all other QC scan data for sites that want to collect qc on other scan types
    Former-commit-id: f7b33bf44268e5ba3520ebaa4c5096f1a0f317f0

---
+++ plugin-resources/webapp/xnat/java/org/nrg/xdat/om/base/BaseXnatOtherqcscandata.java
@@ -0,0 +1,41 @@
+/*
+ * GENERATED FILE
+ * Created on Wed Sep 26 16:16:24 CDT 2012
+ *
+ */
+package org.nrg.xdat.om.base;
+import org.nrg.xdat.om.base.auto.*;
+import org.nrg.xft.*;
+import org.nrg.xft.security.UserI;
+
+import java.util.*;
+
+/**
+ * @author XDAT
+ *
+ */
+@SuppressWarnings({"unchecked","rawtypes"})
+public abstract class BaseXnatOtherqcscandata extends AutoXnatOtherqcscandata {
+
+	public BaseXnatOtherqcscandata(ItemI item)
+	{
+		super(item);
+	}
+
+	public BaseXnatOtherqcscandata(UserI user)
+	{
+		super(user);
+	}
+
+	/*
+	 * @deprecated Use BaseXnatOtherqcscandata(UserI user)
+	 **/
+	public BaseXnatOtherqcscandata()
+	{}
+
+	public BaseXnatOtherqcscandata(Hashtable properties, UserI user)
+	{
+		super(properties,user);
+	}
+
+}

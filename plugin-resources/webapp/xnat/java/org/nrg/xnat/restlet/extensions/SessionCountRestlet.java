aebce9dfeb25913b49dc4f8cf532d33f2e769a7e
Rick Herrick <rick.herrick@wustl.edu>
2012-10-24 17:03:04 -0500

    Added session count restlet for Tony's use on pipeline scripts.
    Former-commit-id: 9db8cac121582c47f709f9b71fb03aea928db75d

---
+++ plugin-resources/webapp/xnat/java/org/nrg/xnat/restlet/extensions/SessionCountRestlet.java
@@ -0,0 +1,67 @@
+/**
+ * SessionCountRestlet
+ * (C) 2012 Washington University School of Medicine
+ * All Rights Reserved
+ *
+ * Released under the Simplified BSD License
+ *
+ * Created on 10/24/12 by rherri01
+ */
+package org.nrg.xnat.restlet.extensions;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.nrg.xdat.XDAT;
+import org.nrg.xdat.security.XDATUser;
+import org.nrg.xnat.restlet.XnatRestlet;
+import org.nrg.xnat.restlet.resources.SecureResource;
+import org.restlet.Context;
+import org.restlet.data.MediaType;
+import org.restlet.data.Request;
+import org.restlet.data.Response;
+import org.restlet.resource.Representation;
+import org.restlet.resource.ResourceException;
+import org.restlet.resource.StringRepresentation;
+import org.restlet.resource.Variant;
+import org.springframework.security.core.session.SessionInformation;
+import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.security.core.session.SessionRegistryImpl;
+
+import java.util.List;
+
+/**
+ * Restlet for returning the number of sessions for a particular user.
+ */
+@XnatRestlet("/services/sessions")
+public class SessionCountRestlet extends SecureResource {
+    private static final Log _log = LogFactory.getLog(SessionCountRestlet.class);
+
+    public SessionCountRestlet(Context context, Request request, Response response) {
+        super(context, request, response);
+        this.getVariants().add(new Variant(MediaType.ALL));
+    }
+
+    @Override
+    public Representation represent(Variant variant) throws ResourceException {
+        if (_log.isDebugEnabled()) {
+            _log.debug("Entering the session count represent() method");
+        }
+
+        int sessionCount = getSessionCount(user);
+        return new StringRepresentation(Integer.toString(sessionCount));
+    }
+
+    private int getSessionCount(XDATUser user) {
+        SessionRegistry sessionRegistry = XDAT.getContextService().getBean("sessionRegistry", SessionRegistryImpl.class);
+
+        int sessionCount = 0;
+
+        if(sessionRegistry != null){
+            List<SessionInformation> l = sessionRegistry.getAllSessions(user, false);
+            if(l != null){
+                sessionCount = l.size();
+            }
+        }
+        return sessionCount;
+    }
+}

16cab3a6945a1ee8dd12f35e44d7280c28a4ccd2
Kevin A. Archie <karchie@wustl.edu>
2012-11-28 14:41:41 -0600

    add xnat:petmrSessionData type and metadata translation
    Former-commit-id: 39ccc6a0ca1f799ea271f42c4b4612548fd28b49

---
+++ plugin-resources/webapp/xnat/java/org/nrg/xdat/om/base/BaseXnatPetmrsessiondata.java
@@ -0,0 +1,41 @@
+/*
+ * GENERATED FILE
+ * Created on Wed Nov 28 13:04:42 CST 2012
+ *
+ */
+package org.nrg.xdat.om.base;
+import org.nrg.xdat.om.base.auto.*;
+import org.nrg.xft.*;
+import org.nrg.xft.security.UserI;
+
+import java.util.*;
+
+/**
+ * @author XDAT
+ *
+ */
+@SuppressWarnings({"unchecked","rawtypes"})
+public abstract class BaseXnatPetmrsessiondata extends AutoXnatPetmrsessiondata {
+
+	public BaseXnatPetmrsessiondata(ItemI item)
+	{
+		super(item);
+	}
+
+	public BaseXnatPetmrsessiondata(UserI user)
+	{
+		super(user);
+	}
+
+	/*
+	 * @deprecated Use BaseXnatPetmrsessiondata(UserI user)
+	 **/
+	public BaseXnatPetmrsessiondata()
+	{}
+
+	public BaseXnatPetmrsessiondata(Hashtable properties, UserI user)
+	{
+		super(properties,user);
+	}
+
+}

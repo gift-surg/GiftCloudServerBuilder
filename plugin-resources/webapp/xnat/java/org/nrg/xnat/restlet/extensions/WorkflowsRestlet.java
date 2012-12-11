9576150a273224f8c269ef038f23473879015b49
Mohana Ramaratnam <mohanakannan9@gmail.com>
2012-12-12 02:03:22 +0530

    Workflow Restlet to query the status of an experiment
    Former-commit-id: 04edbb5bab5f571ae9156b49477ff7c2c0cad7f1

--- plugin-resources/webapp/xnat/java/org/nrg/xnat/restlet/extensions/WorkflowsRestlet.java
+++ plugin-resources/webapp/xnat/java/org/nrg/xnat/restlet/extensions/WorkflowsRestlet.java
@@ -1,12 +1,14 @@
 package org.nrg.xnat.restlet.extensions;
 
 import java.util.ArrayList;
+import java.util.Hashtable;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.nrg.xdat.om.XnatExperimentdata;
 import org.nrg.xdat.om.XnatSubjectassessordata;
 import org.nrg.xft.XFTItem;
+import org.nrg.xft.XFTTable;
 import org.nrg.xft.search.CriteriaCollection;
 import org.nrg.xnat.restlet.XnatRestlet;
 import org.nrg.xnat.restlet.resources.SecureResource;
@@ -23,47 +25,54 @@ import org.restlet.resource.Variant;
  * One can filter the workflows down to the pipeline_name and get the status
  * of the pipeline completion for the experiment
  */
-
-@XnatRestlet({"/archive/workflows", "/archive/workflows/{PIPELINE_NAME}"})
-
 /**
  * Uses:
- * /data/archive/workflows/pipeline_name?project=PROJECT_ID&experiment=PROJECT_LABEL
+ * /data/archive/workflows/pipeline_name?project=PROJECT_ID&experiment=PROJECT_LABEL&tatus=Running&display=Latest
  * /data/archive/workflows/pipeline_name?experiment=XNAT_ID
  * TODO: /data/archive/workflows?subject=XNAT_ID
  * TODO: /data/archive/workflows/pipeline_name upload a list file of XNAT session ids
  */
 
+
+@XnatRestlet({"/services/workflows","/services/workflows/{PIPELINE_NAME}"})
 public class WorkflowsRestlet extends SecureResource {
     private static final Log _log = LogFactory.getLog(WorkflowsRestlet.class);
 
 	String pipeline_name = null;
 	String project_id = null;
 	String xnat_id = null;
-
+	String status = null;
+	final static String DISPLAY_LATEST = "LATEST";
+	final static String DISPLAY_ALL = "ALL";
+	String display = DISPLAY_LATEST;
 	public WorkflowsRestlet(Context context, Request request, Response response) {
         super(context, request, response);
-		String pipeline_name= (String)getParameter(request,"PIPELINE_NAME");
-
+		pipeline_name= (String)getParameter(request,"PIPELINE_NAME");
+		project_id = this.getQueryVariable("project");
+		status = this.getQueryVariable("status");
+		display = this.getQueryVariable("display");
+		if (display == null) 
+			display = DISPLAY_LATEST;
+		
 		if(pipeline_name != null){
 			project_id = this.getQueryVariable("project");
 			xnat_id = this.getQueryVariable("experiment");
 			if (project_id !=null && xnat_id != null) {
-				this.getVariants().add(new Variant(MediaType.TEXT_XML));
+				if (display.equalsIgnoreCase(DISPLAY_LATEST))
+				  this.getVariants().add(new Variant(MediaType.TEXT_XML));
 				this.getVariants().add(new Variant(MediaType.APPLICATION_JSON));
-//				this.getVariants().add(new Variant(MediaType.APPLICATION_EXCEL));
 			}else if (project_id == null && xnat_id != null) {
-				this.getVariants().add(new Variant(MediaType.TEXT_XML));
+				if (display.equalsIgnoreCase(DISPLAY_LATEST))
+					this.getVariants().add(new Variant(MediaType.TEXT_XML));
 				this.getVariants().add(new Variant(MediaType.APPLICATION_JSON));
-//				this.getVariants().add(new Variant(MediaType.APPLICATION_EXCEL));
-//			}else if (project_id == null && xnat_id == null && ) {
-//				this.getVariants().add(new Variant(MediaType.APPLICATION_EXCEL));
-//				this.getVariants().add(new Variant(MediaType.APPLICATION_JSON));
 			}
-		}else{
+		}else if (status != null) {
+			this.getVariants().add(new Variant(MediaType.APPLICATION_JSON));
+		}else {
 		    response.setStatus(Status.CLIENT_ERROR_NOT_FOUND);
 	    }
-    }
+		System.out.println("WorkflowRestlet invoked " + status + " " + pipeline_name + " " + xnat_id + " " + project_id);
+	}
 
 	@Override
 	public Representation getRepresentation(Variant variant) {
@@ -80,20 +89,48 @@ public class WorkflowsRestlet extends SecureResource {
 
 		if(expt!=null){
 		   try {
-			   org.nrg.xft.search.CriteriaCollection cc = new CriteriaCollection("AND");
+			    org.nrg.xft.search.CriteriaCollection cc = new CriteriaCollection("AND");
 		        cc.addClause("wrk:workflowData.ID",expt.getId());
+		        if (status != null)
+		          cc.addClause("wrk:workflowData.status",status);	
 		        //cc.addClause("wrk:workflowData.ExternalID",expt.getProject());
-		        cc.addClause("wrk:workflowData.pipeline_name","LIKE","%/"+pipeline_name+"%");
-		        org.nrg.xft.collections.ItemCollection items = org.nrg.xft.search.ItemSearch.GetItems(cc,user,false);
-	            ArrayList workitems = items.getItems("wrk:workflowData.launch_time","DESC");
-	            XFTItem latestWrkFlow = (XFTItem)workitems.get(0);
-				return representItem(latestWrkFlow.getItem(), mt);
-		   }catch(Exception e) {
-			   logger.error(e);
+		        cc.addClause("wrk:workflowData.pipeline_name","LIKE","%"+pipeline_name.toLowerCase()+"%");
+	            if (display.equalsIgnoreCase(DISPLAY_LATEST)) {
+	            	org.nrg.xft.collections.ItemCollection items = org.nrg.xft.search.ItemSearch.GetItems(cc, user, false);
+		            ArrayList workitems = items.getItems("wrk:workflowData.launch_time","DESC");
+	            	XFTItem latestWrkFlow = (XFTItem)workitems.get(0);
+				    return representItem(latestWrkFlow.getItem(), mt);
+	            }else {
+			        org.nrg.xft.search.ItemSearch itemSearch = new org.nrg.xft.search.ItemSearch(user, "wrk:workflowdata", cc);
+	            	XFTTable table = itemSearch.executeToTable(false);
+	        		Hashtable<String,Object> params=new Hashtable<String,Object>();
+	            	params.put("title", "All workflows");
+	            	return representTable(table, mt, params);
+	            }
+	       }catch(Exception e) {
+			   _log.error(e);
 				this.getResponse().setStatus(Status.CLIENT_ERROR_NOT_FOUND,
 				"Unable to find workflow entry for pipeline " + pipeline_name + " and experiment." + expt.getId());
 				return null;
 		   }
+		}else if (status != null) {
+		    org.nrg.xft.search.CriteriaCollection cc = new CriteriaCollection("AND");
+	        cc.addClause("wrk:workflowData.status",status);
+	        if (pipeline_name != null) {
+	        	cc.addClause("wrk:workflowData.pipeline_name","LIKE","%/"+pipeline_name+"%");
+	        }
+	        try {
+		        org.nrg.xft.search.ItemSearch itemSearch = new org.nrg.xft.search.ItemSearch(user, "wrk:workflowdata", cc);
+            	XFTTable table = itemSearch.executeToTable(false);
+        		Hashtable<String,Object> params=new Hashtable<String,Object>();
+            	params.put("title", "All " + status + " workflows");
+            	return representTable(table, mt, params);
+	        }catch(Exception e) {
+				   logger.error(e);
+					this.getResponse().setStatus(Status.CLIENT_ERROR_NOT_FOUND,
+					"Unable to find workflow entry with status " + status);
+					return null;
+	        }
 		}else{
 			this.getResponse().setStatus(Status.CLIENT_ERROR_NOT_FOUND,
 					"Unable to find the specified experiment.");
@@ -103,6 +140,11 @@ public class WorkflowsRestlet extends SecureResource {
 
 	@Override
 	public boolean allowPost() {
+		return false;
+	}
+	
+	@Override
+	public boolean allowPut() {
 		return false;
 	}
 

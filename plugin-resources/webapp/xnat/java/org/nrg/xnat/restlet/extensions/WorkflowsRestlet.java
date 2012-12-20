a593d9bee15cebebf54c0ac36dab759a93142cee
Mohana Ramaratnam <mohanakannan9@gmail.com>
2012-12-20 23:07:00 +0530

    Added Param query
    Former-commit-id: 7164580b7ff68b1d1f7946817336d6bdb00f3888

--- plugin-resources/webapp/xnat/java/org/nrg/xnat/restlet/extensions/WorkflowsRestlet.java
+++ plugin-resources/webapp/xnat/java/org/nrg/xnat/restlet/extensions/WorkflowsRestlet.java
@@ -2,6 +2,7 @@ package org.nrg.xnat.restlet.extensions;
 
 import java.util.ArrayList;
 import java.util.Hashtable;
+import java.util.Map;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -13,6 +14,7 @@ import org.nrg.xft.search.CriteriaCollection;
 import org.nrg.xnat.restlet.XnatRestlet;
 import org.nrg.xnat.restlet.resources.SecureResource;
 import org.restlet.Context;
+import org.restlet.data.Form;
 import org.restlet.data.MediaType;
 import org.restlet.data.Request;
 import org.restlet.data.Response;
@@ -29,6 +31,8 @@ import org.restlet.resource.Variant;
  * Uses:
  * /data/archive/workflows/pipeline_name?project=PROJECT_ID&experiment=PROJECT_LABEL&tatus=Running&display=Latest
  * /data/archive/workflows/pipeline_name?experiment=XNAT_ID
+ * /data/archive/workflows/pipeline_name?experiment=XNAT_ID&param_NAME=STR&param_NAME_VALUE=STR
+
  * TODO: /data/archive/workflows?subject=XNAT_ID
  * TODO: /data/archive/workflows/pipeline_name upload a list file of XNAT session ids
  */
@@ -45,6 +49,7 @@ public class WorkflowsRestlet extends SecureResource {
 	final static String DISPLAY_LATEST = "LATEST";
 	final static String DISPLAY_ALL = "ALL";
 	String display = DISPLAY_LATEST;
+	Map<String,Object> form_params = null;
 	public WorkflowsRestlet(Context context, Request request, Response response) {
         super(context, request, response);
 		pipeline_name= (String)getParameter(request,"PIPELINE_NAME");
@@ -57,6 +62,8 @@ public class WorkflowsRestlet extends SecureResource {
 		if(pipeline_name != null){
 			project_id = this.getQueryVariable("project");
 			xnat_id = this.getQueryVariable("experiment");
+			form_params = this.getQueryVariablesAsMap();
+
 			if (project_id !=null && xnat_id != null) {
 				if (display.equalsIgnoreCase(DISPLAY_LATEST))
 				  this.getVariants().add(new Variant(MediaType.TEXT_XML));
@@ -90,12 +97,14 @@ public class WorkflowsRestlet extends SecureResource {
 		if(expt!=null){
 		   try {
 			    org.nrg.xft.search.CriteriaCollection cc = new CriteriaCollection("AND");
-		        cc.addClause("wrk:workflowData.ID",expt.getId());
+			    addWorkflowParamsConstraints(cc);
+			    cc.addClause("wrk:workflowData.ID",expt.getId());
 		        if (status != null)
 		          cc.addClause("wrk:workflowData.status",status);	
 		        //cc.addClause("wrk:workflowData.ExternalID",expt.getProject());
 		        cc.addClause("wrk:workflowData.pipeline_name","LIKE","%"+pipeline_name.toLowerCase()+"%");
-	            if (display.equalsIgnoreCase(DISPLAY_LATEST)) {
+		        
+		        if (display.equalsIgnoreCase(DISPLAY_LATEST)) {
 	            	org.nrg.xft.collections.ItemCollection items = org.nrg.xft.search.ItemSearch.GetItems(cc, user, false);
 		            ArrayList workitems = items.getItems("wrk:workflowData.launch_time","DESC");
 	            	XFTItem latestWrkFlow = (XFTItem)workitems.get(0);
@@ -154,4 +163,19 @@ public class WorkflowsRestlet extends SecureResource {
 	}
 
 
+	private void addWorkflowParamsConstraints(CriteriaCollection cc) {
+		if (form_params.size()>0) {
+		for(String s:form_params.keySet()){
+			if (s.startsWith("param_")) {
+				String wrkflow_paramName = s.substring(6);
+				String wrkflow_paramValue = (String)form_params.get(s);
+				cc.addClause("wrk:workflowData.executionEnvironment.parameters.parameter.name", s);
+				cc.addClause("wrk:workflowData.executionEnvironment.parameters.parameter",wrkflow_paramValue);
+
+			}
+		}
+		}
+
+	}
+	
 }

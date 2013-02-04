87571f3505fa74ffd4363a271a5fa67d53509291
Evan Haas <haase@mir.wustl.edu>
2013-02-04 11:16:07 -0600

    XNAT-1641, Part I (add support for SR image modality)
    Former-commit-id: 361edb8cacbf9000dd49326e9e240144e28f34eb

---
+++ plugin-resources/webapp/xnat/java/org/nrg/xdat/om/base/BaseXnatSrscandata.java
@@ -0,0 +1,41 @@
+/*
+ * GENERATED FILE
+ * Created on Thu Jan 31 10:55:11 CST 2013
+ *
+ */
+package org.nrg.xdat.om.base;
+import org.nrg.xdat.om.base.auto.*;
+import org.nrg.xft.*;
+import org.nrg.xft.security.UserI;
+
+import java.util.*;
+
+/**
+ * @author XDAT
+ *
+ */
+@SuppressWarnings({"unchecked","rawtypes"})
+public abstract class BaseXnatSrscandata extends AutoXnatSrscandata {
+
+	public BaseXnatSrscandata(ItemI item)
+	{
+		super(item);
+	}
+
+	public BaseXnatSrscandata(UserI user)
+	{
+		super(user);
+	}
+
+	/*
+	 * @deprecated Use BaseXnatSrscandata(UserI user)
+	 **/
+	public BaseXnatSrscandata()
+	{}
+
+	public BaseXnatSrscandata(Hashtable properties, UserI user)
+	{
+		super(properties,user);
+	}
+
+}
---
+++ plugin-resources/webapp/xnat/java/org/nrg/xdat/om/base/BaseXnatSrsessiondata.java
@@ -0,0 +1,41 @@
+/*
+ * GENERATED FILE
+ * Created on Thu Jan 31 10:55:11 CST 2013
+ *
+ */
+package org.nrg.xdat.om.base;
+import org.nrg.xdat.om.base.auto.*;
+import org.nrg.xft.*;
+import org.nrg.xft.security.UserI;
+
+import java.util.*;
+
+/**
+ * @author XDAT
+ *
+ */
+@SuppressWarnings({"unchecked","rawtypes"})
+public abstract class BaseXnatSrsessiondata extends AutoXnatSrsessiondata {
+
+	public BaseXnatSrsessiondata(ItemI item)
+	{
+		super(item);
+	}
+
+	public BaseXnatSrsessiondata(UserI user)
+	{
+		super(user);
+	}
+
+	/*
+	 * @deprecated Use BaseXnatSrsessiondata(UserI user)
+	 **/
+	public BaseXnatSrsessiondata()
+	{}
+
+	public BaseXnatSrsessiondata(Hashtable properties, UserI user)
+	{
+		super(properties,user);
+	}
+
+}

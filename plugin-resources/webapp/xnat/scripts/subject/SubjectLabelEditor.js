edd327497edfeaf75a9a1dbefff6539b72fe086e
Evan Haas <haase@mir.wustl.edu>
2013-03-13 16:35:40 -0500

    XNAT-1746 (Subject label should be changed via REST and not the subject edit page)
    Former-commit-id: b609be2e3c7df5a6976f6688b51fdd910434441e

---
+++ plugin-resources/webapp/xnat/scripts/subject/SubjectLabelEditor.js
@@ -0,0 +1,234 @@
+/*jslint white: true, vars: true, browser: true */
+/*global XNAT, YAHOO, alert, openModalPanel, closeModalPanel, confirm */
+XNAT.app.SubjectLabelEditor = function(project) {"use strict";
+
+	var that = this;
+	this.labelInput = null;
+	this.startingLabel = null;
+	this.modifiedLabel = null;
+	this.header = XNAT.app.displayNames.singular.subject;
+	this.panel = null;
+	this.onModification = new YAHOO.util.CustomEvent("labelModification", this);
+	this.existingSubjectList = null;
+	this.onExistingSubjectLoadComplete = new YAHOO.util.CustomEvent("load-complete", this);
+
+	this.render = function() {
+		if (!this.panel) {
+			var bd = document.createElement("form");
+			bd.setAttribute("onSubmit", "return(false);");
+			bd.setAttribute("action", "");
+			bd.setAttribute("method", "");
+			var table = document.createElement("table");
+			var tb = document.createElement("tbody");
+			table.appendChild(tb);
+			bd.appendChild(table);
+
+			this.labelInput = document.createElement("input");
+			this.labelInput.id = "new_label";
+			this.labelInput.name = "new_label";
+
+			var labelContainer = document.createElement("div");
+			labelContainer.id = "complete_container";
+			labelContainer.width = "100px";
+			labelContainer.appendChild(this.labelInput);
+
+			//modality
+			var tr = document.createElement("tr");
+			var td1 = document.createElement("th");
+			var td2 = document.createElement("td");
+			var td3 = document.createElement("td");
+
+			td1.innerHTML = this.header + ":";
+			td1.align = "left";
+
+			td2.appendChild(labelContainer);
+
+			tr.appendChild(td1);
+			tr.appendChild(td2);
+			tb.appendChild(tr);
+
+			this.panel = new YAHOO.widget.Dialog("subjectLabelDialog", {
+				close : true,
+				//width:"350px",
+				//zIndex:9,
+				underlay : "shadow",
+				modal : true,
+				fixedCenter : true,
+				visible : false
+			});
+			this.panel.handleEnter = function() {
+				var label = this.form.new_label;
+				that.modifiedLabel = label.value.trim();
+				if (that.modifiedLabel === "") {
+					alert("Please specify a new ID.");
+					label.focus();
+				} else if (that.modifiedLabel === that.startingLabel) {
+					alert("No modification found.");
+					label.focus();
+				} else {
+					var validatedLabel = that.cleanLabel(that.modifiedLabel);
+					if (validatedLabel !== that.modifiedLabel) {
+						label.value = validatedLabel;
+						alert("Invalid characters in new ID.  Review modified value and resubmit.");
+						label.focus();
+						return;
+					}
+
+					var lC;
+					for (lC = 0; lC < that.existingSubjectList.length; lC = lC + 1) {
+						if (that.modifiedLabel === that.existingSubjectList[lC].label) {
+							alert("This ID is already in use by a " + that.header + " in this " + XNAT.app.displayNames.singular.project.toLowerCase() + ".  Please modify and resubmit.");
+							label.focus();
+							return;
+						}
+					}
+
+					if (confirm("Modifying the label of a " + that.header + " will result in the moving of files on the file server within the " + XNAT.app.displayNames.singular.project.toLowerCase() + "'s storage space.  Are you sure you want to make this change?")) {
+						that.modifyLabel();
+					}
+				}
+			};
+			this.panel.handleCancel = function() {
+				this.cancel();
+			};
+
+			var buttons = [{
+				text : "Modify",
+				handler : {
+					fn : this.panel.handleEnter
+				},
+				isDefault : true
+			}, {
+				text : "Cancel",
+				handler : {
+					fn : this.panel.handleCancel
+				}
+			}];
+
+			var cancelListener = new YAHOO.util.KeyListener(document, {
+				keys : 27
+			}, {
+				fn : this.panel.handleCancel,
+				scope : this.panel,
+				correctScope : true
+			});
+			var enterListener = new YAHOO.util.KeyListener(document, {
+				keys : 13
+			}, {
+				fn : this.panel.handleEnter,
+				scope : this.panel,
+				correctScope : true
+			});
+
+			this.panel.setHeader("Label modification");
+			this.panel.setBody(bd);
+			this.panel.form = bd;
+			this.panel.selector = this;
+			this.panel.cfg.queueProperty("keyListeners", [cancelListener, enterListener]);
+			this.panel.cfg.queueProperty("buttons", buttons);
+			this.panel.render("page_body");
+		}
+	};
+
+	this.loadSubjects = function() {
+
+		function displayError(errorMsg) {
+			alert(errorMsg);
+		}
+
+		function initFailure(o) {
+			if (!window.leaving) {
+				displayError("ERROR " + o.status + ": Failed to load " + XNAT.app.displayNames.singular.subject.toLowerCase() + " list.");
+			}
+		}
+
+		function completeInit(o) {
+			try {
+				that.existingSubjectList = YAHOO.lang.JSON.parse(o.responseText).ResultSet.Result;
+				that.onExistingSubjectLoadComplete.fire();
+			} catch (e) {
+				if (o.status !== 200) {
+					displayError("ERROR " + o.status + ": Failed to parse " + XNAT.app.displayNames.singular.subject.toLowerCase() + " list.");
+				} else {
+					displayError("EXCEPTION: " + e.toString());
+				}
+			}
+
+		}
+
+		var initCallback = {
+			success : completeInit,
+			failure : initFailure,
+			cache : false, // Turn off caching for IE
+			scope : this
+		};
+
+		openModalPanel("labels_loading", "Loading " + this.header + "s...");
+		YAHOO.util.Connect.asyncRequest('GET', window.serverRoot + '/REST/projects/' + project + '/subjects?XNAT_CSRF=' + window.csrfToken + '&format=json&timestamp=' + (new Date()).getTime(), initCallback, null, this);
+	};
+
+	this.onExistingSubjectLoadComplete.subscribe(function() {
+		closeModalPanel("labels_loading");
+		that.panel.show();
+	});
+
+	this.show = function(currentLabel) {
+		this.labelInput.value = currentLabel;
+		this.startingLabel = currentLabel;
+		this.loadSubjects();
+	};
+
+	this.cleanLabel = function(val) {
+		var temp = val.replace(/^\s*|\s*$/g, "");
+		var newVal = "";
+		temp = temp.split(' ');
+		var c;
+		for ( c = 0; c < temp.length; c = c + 1) {
+			newVal += temp[c];
+		}
+
+		newVal = newVal.replace(/[&]/, "_");
+		newVal = newVal.replace(/[?]/, "_");
+		newVal = newVal.replace(/[<]/, "_");
+		newVal = newVal.replace(/[>]/, "_");
+		newVal = newVal.replace(/[(]/, "_");
+		newVal = newVal.replace(/[)]/, "_");
+		newVal = newVal.replace(/[#]/, "_");
+		newVal = newVal.replace(/[%]/, "_");
+		newVal = newVal.replace(/[=]/, "_");
+		newVal = newVal.replace(/[{]/, "_");
+		newVal = newVal.replace(/[}]/, "_");
+		newVal = newVal.replace(/[|]/, "_");
+		newVal = newVal.replace(/[,]/, "_");
+		newVal = newVal.replace(/[`]/, "_");
+		newVal = newVal.replace(/[~]/, "_");
+		newVal = newVal.replace(/[;]/, "_");
+		newVal = newVal.replace(/[:]/, "_");
+		return newVal;
+	};
+
+	this.modifyLabel = function() {
+		var settingsCallback = {
+			success : function(o) {
+				closeModalPanel("modify_new_label");
+				that.onModification.fire(that.modifiedLabel);
+				that.panel.cancel();
+			},
+			failure : function(o) {
+				if (!window.leaving) {
+					alert("ERROR (" + o.status + "): Failed to modify " + that.header + " ID:\n\n" + o.responseText);
+					closeModalPanel("modify_new_label");
+				}
+			},
+			scope : this
+		};
+
+		var params = "";
+		params += "&event_type=WEB_FORM";
+		params += "&event_action=Modified label";
+
+		openModalPanel("modify_new_label", "Modifying " + this.header + ", please wait...");
+		var uri = window.serverRoot + "/REST/projects/" + project +"/subjects/" + this.startingLabel;
+		YAHOO.util.Connect.asyncRequest('PUT', uri + "?label=" + this.modifiedLabel + "&XNAT_CSRF=" + window.csrfToken + params, settingsCallback);
+	};
+};

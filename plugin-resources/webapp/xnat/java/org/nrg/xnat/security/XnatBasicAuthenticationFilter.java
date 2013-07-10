e82eccb9e84b1d6092b252ef5ec3ac57b1b1a9b2
John Smith <smithj@mir.wustl.edu>
2013-07-10 12:53:09 -0500

    XNAT-2186 XnatBasicAuthenticaionFilter has been altered to record an entry in the xdat_user_login table so as to track last login for all REST calls that require authentication.
    Former-commit-id: c74b3fd73eb86891d7defcb8dff165b1ba88f18b

--- plugin-resources/webapp/xnat/java/org/nrg/xnat/security/XnatBasicAuthenticationFilter.java
+++ plugin-resources/webapp/xnat/java/org/nrg/xnat/security/XnatBasicAuthenticationFilter.java
@@ -1,6 +1,7 @@
 package org.nrg.xnat.security;
 
 import java.io.IOException;
+import java.util.UUID;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -9,13 +10,19 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.nrg.xdat.entities.XDATUserDetails;
+import org.nrg.xdat.turbine.utils.AccessLogger;
 import org.nrg.xdat.turbine.utils.AdminUtils;
+import org.nrg.xft.XFTItem;
+import org.nrg.xft.event.EventUtils;
+import org.nrg.xft.utils.SaveItemHelper;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.authentication.AuthenticationDetailsSource;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.codec.Base64;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.security.web.authentication.session.NullAuthenticatedSessionStrategy;
@@ -117,6 +124,41 @@ public class XnatBasicAuthenticationFilter extends BasicAuthenticationFilter {
 
         chain.doFilter(request, response);
 	}
+
+    @Override
+    // XNAT-2186 requested that REST logins also leave records of last login date
+    protected void onSuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response,
+                                              Authentication authResult) throws IOException {
+        try{
+            SecurityContext securityContext = SecurityContextHolder.getContext();
+
+            XDATUserDetails user= null;
+            Object principal = securityContext.getAuthentication().getPrincipal();
+
+            if(principal instanceof XDATUserDetails){
+                user = (XDATUserDetails)principal;
+            }
+            else if (principal instanceof String){
+                user = new XDATUserDetails((String)principal);
+            }
+
+            request.getSession().setAttribute("user", user);
+            java.util.Date today = java.util.Calendar.getInstance(java.util.TimeZone.getDefault()).getTime();
+            XFTItem item = XFTItem.NewItem("xdat:user_login",user);
+            item.setProperty("xdat:user_login.user_xdat_user_id", user.getID());
+            item.setProperty("xdat:user_login.login_date",today);
+            item.setProperty("xdat:user_login.ip_address", AccessLogger.GetRequestIp(request));
+            item.setProperty("xdat:user_login.session_id", request.getSession().getId());
+            SaveItemHelper.authorizedSave(item, null, true, false, EventUtils.DEFAULT_EVENT(user, null));
+
+            request.getSession().setAttribute("XNAT_CSRF", UUID.randomUUID().toString());
+        }
+        catch(Exception e){
+            logger.error(e);
+        }
+
+        super.onSuccessfulAuthentication(request, response, authResult);
+    }
     
     /**
      * The session handling strategy which will be invoked immediately after an authentication request is

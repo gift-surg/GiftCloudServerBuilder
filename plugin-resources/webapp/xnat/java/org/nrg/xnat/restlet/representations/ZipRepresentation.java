bf2e238b8f9c5e550feb37a9549791cf9ef9ded9
Kevin A. Archie <karchie@wustl.edu>
2013-07-17 14:45:32 -0500

    clean up java.io.tmpdir after DICOMDIR creation
    Former-commit-id: 5b096472e78cb80a641cb37f329f95717a42087a

--- plugin-resources/webapp/xnat/java/org/nrg/xnat/restlet/representations/ZipRepresentation.java
+++ plugin-resources/webapp/xnat/java/org/nrg/xnat/restlet/representations/ZipRepresentation.java
@@ -10,152 +10,162 @@
  */
 package org.nrg.xnat.restlet.representations;
 
-import org.nrg.xft.utils.zip.TarUtils;
-import org.nrg.xft.utils.zip.ZipI;
-import org.nrg.xft.utils.zip.ZipUtils;
-import org.nrg.xnat.restlet.resources.SecureResource;
-import org.restlet.data.MediaType;
-import org.restlet.resource.OutputRepresentation;
-
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 import java.util.zip.ZipOutputStream;
 
-public class ZipRepresentation extends OutputRepresentation {
-	private ArrayList<ZipEntry> _entries=new ArrayList<ZipEntry>();
-	private ArrayList<String> _tokens=new ArrayList<String>();
-	private MediaType mt=null;
-	private final int compression;
-	
-	public ZipRepresentation(MediaType mt,String token, Integer compression) {
-		super(mt);
-		this.mt=mt;
-		_tokens.add(token);
-		this.compression=deriveCompression(compression);
-	}
-	
-	public ZipRepresentation(MediaType mt,ArrayList<String> token, Integer compression) {
-		super(mt);
-		this.mt=mt;
-		_tokens=token;
-		this.compression=deriveCompression(compression);
-	}
-	
-	public int deriveCompression(Integer compression){
-		if(compression==null){
-			return ZipUtils.DEFAULT_COMPRESSION;
-		}else{
-			return compression;
-		}
-	}
-	
-	public void addEntry(String p, File f){
-		_entries.add(new ZipFileEntry(p,f));
-	}
-	
-	public void addEntry(String p, InputStream is){
-		_entries.add(new ZipStreamEntry(p,is));
-	}
-	
-	public void addFolder(String p, File dir){
-		if(dir.isDirectory()){
-			for(final File f:dir.listFiles()){
-				if(f.isDirectory()){
-					this.addFolder(append(p,f.getName()),f);
-				}else{
-					this.addEntry(append(p,f.getName()),f);
-				}
-			}
-		}else{
-			this.addEntry(append(p,dir.getName()),dir);
-		}
-	}
-	
-	public String append(String pre, String post){
-		return (pre.endsWith("/"))?pre+post:pre+"/"+post;
-	}
-	
-	public void addEntry(File f){
-		String p = f.getAbsolutePath().replace('\\','/');
-		int i=-1;
-		String _token=null;
-		
-		for(String token:_tokens){
-			_token=token;
-			i=p.indexOf('/'+ _token + '/');
-			if(i==-1){
-				i=p.indexOf('/'+ _token);
-				
-				if(i==-1){
-					i=p.indexOf(_token+'/');
-					if(i>-1){
-						i=(p.substring(0, i)).lastIndexOf('/') +1;
-						break;
-					}
-				}else{
-					i++;
-					break;
-				}
-			}else{
-				i++;
-				break;
-			}
-		}
-		
-		if(i==-1){
-			if(p.indexOf(":")>-1){
-				p=p.substring(p.indexOf(":"));
-				p=p.substring(p.indexOf("/"));
-				p=_token+p;
-			}else{
-				p=_token+p;
-			}
-			
-			_entries.add(new ZipFileEntry(p,f));
-		}else{
-			
-			_entries.add(new ZipFileEntry(p.substring(i),f));
-		}
-	}
-	
-	public void addAll(List<File> fs){
-		for(File f: fs){
-			this.addEntry(f);
-		}
-	}
-	
-	public void addAllAtRelativeDirectory(String ins, ArrayList<File> fs) {
-		ins=ins.replace('\\','/');
-		for(File f: fs){
-			String pathS = f.getAbsolutePath().replace('\\','/');
-			int pos=pathS.indexOf(ins);
-			if (pos>=0) {
-				this.addEntry(pathS.substring(pos+ins.length()+1),f);
-			} else {
-				this.addEntry(f);
-			}
-		}
-	}
-	
-	public String getTokenName(){
-		if(this._tokens.size()>1){
-			return "various";
-		}else{
-			return this._tokens.get(0);
-		}
-	}
-	
-	public int getEntryCount(){
-		return this._entries.size();
-	}
+import org.apache.commons.io.FileUtils;
+import org.nrg.xft.utils.zip.TarUtils;
+import org.nrg.xft.utils.zip.ZipI;
+import org.nrg.xft.utils.zip.ZipUtils;
+import org.nrg.xnat.restlet.resources.SecureResource;
+import org.restlet.data.MediaType;
+import org.restlet.resource.OutputRepresentation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-	@Override
-	public String getDownloadName() {
-		if (this.mt.equals(MediaType.APPLICATION_GNU_TAR))
+import com.google.common.collect.Lists;
+
+public class ZipRepresentation extends OutputRepresentation {
+    private final Logger logger = LoggerFactory.getLogger(ZipRepresentation.class);
+    
+    private ArrayList<ZipEntry> _entries=new ArrayList<ZipEntry>();
+    private ArrayList<String> _tokens=new ArrayList<String>();
+    private MediaType mt=null;
+    private final int compression;
+    private List<Runnable> afterWrite = Lists.newArrayList();
+
+    public ZipRepresentation(MediaType mt,String token, Integer compression) {
+        super(mt);
+        this.mt=mt;
+        _tokens.add(token);
+        this.compression=deriveCompression(compression);
+    }
+
+    public ZipRepresentation(MediaType mt,ArrayList<String> token, Integer compression) {
+        super(mt);
+        this.mt=mt;
+        _tokens=token;
+        this.compression=deriveCompression(compression);
+    }
+
+    public int deriveCompression(Integer compression){
+        if(compression==null){
+            return ZipUtils.DEFAULT_COMPRESSION;
+        }else{
+            return compression;
+        }
+    }
+
+    public void addEntry(String p, File f){
+        _entries.add(new ZipFileEntry(p,f));
+    }
+
+    public void addEntry(String p, InputStream is){
+        _entries.add(new ZipStreamEntry(p,is));
+    }
+
+    public void addFolder(String p, File dir){
+        if(dir.isDirectory()){
+            for(final File f:dir.listFiles()){
+                if(f.isDirectory()){
+                    this.addFolder(append(p,f.getName()),f);
+                }else{
+                    this.addEntry(append(p,f.getName()),f);
+                }
+            }
+        }else{
+            this.addEntry(append(p,dir.getName()),dir);
+        }
+    }
+
+    public String append(String pre, String post){
+        return (pre.endsWith("/"))?pre+post:pre+"/"+post;
+    }
+
+    public void addEntry(File f){
+        String p = f.getAbsolutePath().replace('\\','/');
+        int i=-1;
+        String _token=null;
+
+        for(String token:_tokens){
+            _token=token;
+            i=p.indexOf('/'+ _token + '/');
+            if(i==-1){
+                i=p.indexOf('/'+ _token);
+
+                if(i==-1){
+                    i=p.indexOf(_token+'/');
+                    if(i>-1){
+                        i=(p.substring(0, i)).lastIndexOf('/') +1;
+                        break;
+                    }
+                }else{
+                    i++;
+                    break;
+                }
+            }else{
+                i++;
+                break;
+            }
+        }
+
+        if(i==-1){
+            if(p.indexOf(":")>-1){
+                p=p.substring(p.indexOf(":"));
+                p=p.substring(p.indexOf("/"));
+                p=_token+p;
+            }else{
+                p=_token+p;
+            }
+
+            _entries.add(new ZipFileEntry(p,f));
+        }else{
+
+            _entries.add(new ZipFileEntry(p.substring(i),f));
+        }
+    }
+
+    public void addAll(List<File> fs){
+        for(File f: fs){
+            this.addEntry(f);
+        }
+    }
+
+    public void addAllAtRelativeDirectory(String ins, ArrayList<File> fs) {
+        ins=ins.replace('\\','/');
+        for(File f: fs){
+            String pathS = f.getAbsolutePath().replace('\\','/');
+            int pos=pathS.indexOf(ins);
+            if (pos>=0) {
+                this.addEntry(pathS.substring(pos+ins.length()+1),f);
+            } else {
+                this.addEntry(f);
+            }
+        }
+    }
+
+    public String getTokenName(){
+        if(this._tokens.size()>1){
+            return "various";
+        }else{
+            return this._tokens.get(0);
+        }
+    }
+
+    public int getEntryCount(){
+        return this._entries.size();
+    }
+
+    @Override
+    public String getDownloadName() {
+        if (this.mt.equals(MediaType.APPLICATION_GNU_TAR))
         {
             return getTokenName()+".tar.gz";
         }else if (this.mt.equals(MediaType.APPLICATION_TAR))
@@ -167,88 +177,125 @@ public class ZipRepresentation extends OutputRepresentation {
         }else{
             return getTokenName() +".zip";
         }
-	}
+    }
 
-	@Override
-	public void write(OutputStream os) throws IOException {
-		ZipI zip = null;
-        if (this.mt.equals(MediaType.APPLICATION_GNU_TAR))
-        {
-            zip = new TarUtils();
-            zip.setOutputStream(os,ZipOutputStream.DEFLATED);
-            this.setDownloadName(getTokenName()+".tar.gz");
-            this.setDownloadable(true);
-        }else if (this.mt.equals(MediaType.APPLICATION_TAR)){
-        	zip = new TarUtils();
-            zip.setOutputStream(os,ZipOutputStream.STORED);
-            this.setDownloadName(getTokenName()+".tar");
-            this.setDownloadable(true);
-        }else{
-            zip = new ZipUtils();
-            zip.setOutputStream(os,compression);
-            this.setDownloadName(getTokenName() +".zip");
-            this.setDownloadable(true);
-        }
-            
-        for(final ZipEntry ze: this._entries)
-        {
-        	if(ze instanceof ZipFileEntry){
-                if (!((ZipFileEntry)ze).getF().isDirectory())
-                {
-                    zip.write(ze.getPath(),((ZipFileEntry)ze).getF());
+    /**
+     * Adds a task that should be performed asynchronously after this ZipRepresentation
+     * completes a write.
+     * @param runnable
+     * @return this
+     */
+    public ZipRepresentation afterWrite(final Runnable runnable) {
+        afterWrite.add(runnable);
+        return this;
+    }
+    
+    /**
+     * After this ZipRepresentation completes a write, remove the named file.
+     * @param f
+     * @return this
+     */
+    public ZipRepresentation deleteDirectoryAfterWrite(final File f) {
+        return afterWrite(new Runnable() {
+            public void run() {
+                try {
+                    FileUtils.deleteDirectory(f);
+                } catch (IOException e) {
+                    logger.error("unable to remove working directory " + f, e);
                 }
-        	}else{
-                zip.write(ze.getPath(),((ZipStreamEntry)ze).getInputStream());
-        	}
+            }
+        });
+    }
+
+    @Override
+    public void write(OutputStream os) throws IOException {
+        try {
+            ZipI zip = null;
+            if (this.mt.equals(MediaType.APPLICATION_GNU_TAR))
+            {
+                zip = new TarUtils();
+                zip.setOutputStream(os,ZipOutputStream.DEFLATED);
+                this.setDownloadName(getTokenName()+".tar.gz");
+                this.setDownloadable(true);
+            }else if (this.mt.equals(MediaType.APPLICATION_TAR)){
+                zip = new TarUtils();
+                zip.setOutputStream(os,ZipOutputStream.STORED);
+                this.setDownloadName(getTokenName()+".tar");
+                this.setDownloadable(true);
+            }else{
+                zip = new ZipUtils();
+                zip.setOutputStream(os,compression);
+                this.setDownloadName(getTokenName() +".zip");
+                this.setDownloadable(true);
+            }
+
+            for (final ZipEntry ze: this._entries) {
+                if (ze instanceof ZipFileEntry) {
+                    final ZipFileEntry zfe = (ZipFileEntry)ze;
+                    final File f = zfe.getF();
+                    if (!f.isDirectory()) {
+                        zip.write(ze.getPath(),f);
+                    }
+                } else {
+                    zip.write(ze.getPath(), ((ZipStreamEntry)ze).getInputStream());
+                }
+            }
+
+            // Complete the ZIP file
+            zip.close();
+        } finally {
+            if (!afterWrite.isEmpty()) {
+                final Executor executor = Executors.newSingleThreadExecutor();
+                for (final Runnable r : afterWrite) {
+                    executor.execute(r);
+                }
+            }
         }
-              
-        // Complete the ZIP file
-        zip.close();
-	}
+    }
 
-	public abstract class ZipEntry{
-		String path=null;
-		
-		public String getPath() {
-			return path;
-		}
-		public void setPath(String path) {
-			this.path = path;
-		}
-	}
-    
-	public class ZipFileEntry extends ZipEntry {
-		public ZipFileEntry(String p,File f){
-			path=p;
-			file=f;
-		}
-		
-		File file=null;
-		
-		public File getF() {
-			return file;
-		}
-		public void setF(File f) {
-			this.file = f;
-		}
-	}
+    public abstract class ZipEntry{
+        String path=null;
 
-    
-	public class ZipStreamEntry extends ZipEntry {
-		public ZipStreamEntry(String p,InputStream _is){
-			path=p;
-			is=_is;
-		}
-		
-		private InputStream is=null;
-		
-		public InputStream getInputStream() {
-			return is;
-		}
-		public void setInputStream(InputStream _is) {
-			this.is = _is;
-		}
-	}
+        public String getPath() {
+            return path;
+        }
+        public void setPath(String path) {
+            this.path = path;
+        }
+    }
+
+    public class ZipFileEntry extends ZipEntry {
+        public ZipFileEntry(String p,File f){
+            path=p;
+            file=f;
+        }
+
+        File file=null;
+
+        public File getF() {
+            return file;
+        }
+        public void setF(File f) {
+            this.file = f;
+        }
+    }
+
+
+    public class ZipStreamEntry extends ZipEntry {
+        public ZipStreamEntry(String p,InputStream _is){
+            path=p;
+            is=_is;
+        }
+
+        private InputStream is=null;
+
+        public InputStream getInputStream() {
+            return is;
+        }
+        public void setInputStream(InputStream _is) {
+            this.is = _is;
+        }
+    }
 }
 
 
--- plugin-resources/webapp/xnat/java/org/nrg/xnat/restlet/resources/ScanDIRResource.java
+++ plugin-resources/webapp/xnat/java/org/nrg/xnat/restlet/resources/ScanDIRResource.java
@@ -5,8 +5,6 @@
  * All Rights Reserved
  *
  * Released under the Simplified BSD.
- *
- * Last modified 7/10/13 8:47 PM
  */
 package org.nrg.xnat.restlet.resources;
 
@@ -25,136 +23,136 @@ import org.restlet.data.Response;
 import org.restlet.data.Status;
 import org.restlet.resource.Representation;
 import org.restlet.resource.Variant;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.base.Strings;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.URLDecoder;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Hashtable;
 import java.util.List;
 import java.util.Map;
 
 public class ScanDIRResource extends ScanResource {
-    final org.apache.log4j.Logger logger = org.apache.log4j.Logger.getLogger(ScanDIRResource.class);
+    final Logger logger = LoggerFactory.getLogger(ScanDIRResource.class);
     public ScanDIRResource(Context context, Request request, Response response) {
-	super(context, request, response);
+        super(context, request, response);
 
-	this.getVariants().clear();
-	this.getVariants().add(new Variant(MediaType.APPLICATION_ZIP));
+        this.getVariants().clear();
+        this.getVariants().add(new Variant(MediaType.APPLICATION_ZIP));
     }
 
 
     @Override
-	public boolean allowPut() {
-	return false;
+    public boolean allowPut() {
+        return false;
     }
 
     @Override
-	public Representation getRepresentation(Variant variant) {
-	List<XnatImagescandata> scans = new ArrayList<XnatImagescandata>();
+    public Representation getRepresentation(Variant variant) {
+        final List<XnatImagescandata> scans;
+        if (null != scan) {
+            scans = Collections.singletonList(scan);
+        } else if (null == this.session) {
+            this.getResponse().setStatus(Status.CLIENT_ERROR_NOT_FOUND, "Unable to find the specified session.");
+            return null;
+        } else if (!Strings.isNullOrEmpty(scanID)) {
+            scanID = URLDecoder.decode(scanID);
+            scans = XnatImagescandata.getScansByIdORType(scanID, session, user, completeDocument);
+            if (scans.isEmpty()){
+                this.getResponse().setStatus(Status.CLIENT_ERROR_NOT_FOUND, "Unable to find the specified scan(s).");
+                return null;
+            }
+        } else {
+            // TODO: use all scans for the given session?
+            this.getResponse().setStatus(Status.CLIENT_ERROR_BAD_REQUEST, "No scan specified");
+            return null;
+        }
+        assert !scans.isEmpty();
 
-	if (scan == null && scanID != null) {
-	    if (scan == null && this.session != null) {
-		scanID = URLDecoder.decode(scanID);
-		scans=XnatImagescandata.getScansByIdORType(scanID, session,user,completeDocument);
-	    } else {
-		if (this.session == null) {
-		    this.getResponse().setStatus(Status.CLIENT_ERROR_NOT_FOUND,
-						 "Unable to find the specified session.");
-		    return null;
-		}
-	    }
-	}
+        ZipRepresentation rep;
+        try {
+            //prepare Maps for use in cleaning file paths and relativing them.
+            final Map<String,String> session_mapping=new Hashtable<String,String>();
+            session_mapping.put(session.getId(),session.getArchiveDirectoryName());
+            session_mapping.put(session.getArchiveDirectoryName(),session.getArchiveDirectoryName());
 
-	if(scans.size()==0){
-	    this.getResponse().setStatus(Status.CLIENT_ERROR_NOT_FOUND,
-					 "Unable to find the specified scan(s).");
-	    return null;
-	}
+            final ArrayList<String> session_ids=new ArrayList<String>();
+            session_ids.add(session.getArchiveDirectoryName());
 
-	ZipRepresentation rep;
-	try {
-	    //prepare Maps for use in cleaning file paths and relativing them.
-	    final Map<String,String> session_mapping=new Hashtable<String,String>();
-	    session_mapping.put(session.getId(),session.getArchiveDirectoryName());
-	    session_mapping.put(session.getArchiveDirectoryName(),session.getArchiveDirectoryName());
+            Map<String,String> valuesToReplace=RestFileUtils.getReMaps(scans,null);
+            try{
+                rep = new ZipRepresentation(MediaType.APPLICATION_ZIP,session_ids,identifyCompression(null));
+            } catch (ActionException e) {
+                logger.error("",e);
+                this.setResponseStatus(e);
+                return null;
+            }
 
-	    final ArrayList<String> session_ids=new ArrayList<String>();
-	    session_ids.add(session.getArchiveDirectoryName());
-
-	    Map<String,String> valuesToReplace=RestFileUtils.getReMaps(scans,null);
-	    try{
-		    rep = new ZipRepresentation(MediaType.APPLICATION_ZIP,session_ids,identifyCompression(null));
-		} catch (ActionException e) {
-			logger.error("",e);
-			this.setResponseStatus(e);
-			return null;
-		}
-
-	    //this is the expected path to the SESSION_DIR
-	    final String rootPath=session.getArchivePath();
+            //this is the expected path to the SESSION_DIR
+            final String rootPath=session.getArchivePath();
 
 
-	    // create a directory in the temporary directory to hold our files
-	    File _tmp_working_dir = File.createTempFile("dicom_","",new File(System.getProperty("java.io.tmpdir")));
-	    String name = _tmp_working_dir.getAbsolutePath();
-	    _tmp_working_dir.delete();
-	    File tmp_working_dir = new File(name);
+            // create a directory in the temporary directory to hold our files
+            File _tmp_working_dir = File.createTempFile("dicom_","",new File(System.getProperty("java.io.tmpdir")));
+            String name = _tmp_working_dir.getAbsolutePath();
+            _tmp_working_dir.delete();
+            final File tmp_working_dir = new File(name);
 
-	    try {
-       		System.out.println("Made directory : " + tmp_working_dir.mkdirs());
+            tmp_working_dir.mkdirs();
 
-		// make the DICOMDIR file inside the working temp directory
-		File dicomDIRFile = new File(tmp_working_dir,"DICOMDIR");
-		dicomDIRFile.createNewFile();
+            // make the DICOMDIR file inside the working temp directory
+            final File dicomDIRFile = new File(tmp_working_dir,"DICOMDIR");
+            dicomDIRFile.createNewFile();
 
-		// populate the DICOMDIR
-		DicomDir dicomdir = new DicomDir(dicomDIRFile);
-		try {
-		    dicomdir.create();
-		    //iterate through scans and only include DICOM files.
-		    for(final XnatImagescandata scan: scans){
-			for(final XnatAbstractresourceI res: scan.getFile()){
-				    if(((XnatAbstractresource)res).getFormat()!=null && ((XnatAbstractresource)res).getFormat().equals("DICOM")){
-				for(final File f:((XnatAbstractresource)res).getCorrespondingFiles(rootPath)){
-				    final String uri=f.getAbsolutePath();
-				    final String relative = RestFileUtils.buildRelativePath(uri, session_mapping, valuesToReplace, res.getXnatAbstractresourceId(), res.getLabel());
-				    // create a matching directory structure in the working temp directory
-				    File tmp_dicom_dir = new File(tmp_working_dir, relative).getParentFile();
-				    if (tmp_dicom_dir != null) {
-					tmp_dicom_dir.mkdirs();
-				    }
-				    if(f!=null && f.exists()){
-					rep.addEntry(relative, f);
-					// copy file to the directory structure in the working temp directory.
-					FileUtils.copyFileToDirectory(f,tmp_dicom_dir);
-					File tmp_dicom_file = new File(tmp_dicom_dir,f.getName());
-					dicomdir.addFile(tmp_dicom_dir);
-					// delete the file now to avoid buildup.
-					tmp_dicom_file.delete();
-				    }
-				}
-			    }
-			}
-		    }
+            // populate the DICOMDIR
+            final DicomDir dicomdir = new DicomDir(dicomDIRFile);
+            try {
+                dicomdir.create();
+                //iterate through scans and only include DICOM files.
+                for(final XnatImagescandata scan: scans){
+                    for(final XnatAbstractresourceI res: scan.getFile()){
+                        if(((XnatAbstractresource)res).getFormat()!=null && ((XnatAbstractresource)res).getFormat().equals("DICOM")){
+                            for(final File f:((XnatAbstractresource)res).getCorrespondingFiles(rootPath)){
+                                final String uri=f.getAbsolutePath();
+                                final String relative = RestFileUtils.buildRelativePath(uri, session_mapping, valuesToReplace, res.getXnatAbstractresourceId(), res.getLabel());
+                                // create a matching directory structure in the working temp directory
+                                File tmp_dicom_dir = new File(tmp_working_dir, relative).getParentFile();
+                                if (tmp_dicom_dir != null) {
+                                    tmp_dicom_dir.mkdirs();
+                                }
+                                if(f!=null && f.exists()){
+                                    rep.addEntry(relative, f);
+                                    // copy file to the directory structure in the working temp directory.
+                                    FileUtils.copyFileToDirectory(f,tmp_dicom_dir);
+                                    File tmp_dicom_file = new File(tmp_dicom_dir,f.getName());
+                                    dicomdir.addFile(tmp_dicom_dir);
+                                    // delete the file now to avoid buildup.
+                                    tmp_dicom_file.delete();
+                                }
+                            }
+                        }
+                    }
+                }
 
-		    rep.addEntry("DICOMDIR", dicomDIRFile);
-		    this.setContentDisposition(rep.getDownloadName());
-		    return rep;
-		}
-		finally {
-		    dicomdir.close();
-		}
-	    }
-	    finally {
-		tmp_working_dir.delete();
-	    }
-
-	}
-	catch (Throwable e) {
-	    logger.error("", e);
-	    this.getResponse().setStatus(Status.SERVER_ERROR_INTERNAL,e.getMessage());
-	    return null;
-	}
+                rep.addEntry("DICOMDIR", dicomDIRFile);
+                rep.deleteDirectoryAfterWrite(tmp_working_dir);
+                this.setContentDisposition(rep.getDownloadName());
+                return rep;
+            }
+            finally {
+                dicomdir.close();
+            }
+        }
+        catch (Throwable e) {
+            logger.error("", e);
+            this.getResponse().setStatus(Status.SERVER_ERROR_INTERNAL,e.getMessage());
+            return null;
+        }
     }
 }
 

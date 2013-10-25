d4e7b5e3b24373de8f3fcdaf2a4bee2d3f4ed036
John Smith <smithj@mir.wustl.edu>
2013-10-25 15:23:12 -0500

    XNAT-2222 Made XnatDatabaseAuthenticationProvider capable of telling between accounts disabled due to activity and accounts disabled by admins.
    Former-commit-id: b649c65f204a85daeb87d00afb735dc8e5635109

--- plugin-resources/webapp/xnat/java/org/nrg/xnat/security/provider/XnatDatabaseAuthenticationProvider.java
+++ plugin-resources/webapp/xnat/java/org/nrg/xnat/security/provider/XnatDatabaseAuthenticationProvider.java
@@ -12,15 +12,22 @@ package org.nrg.xnat.security.provider;
 
 import org.nrg.xdat.entities.XDATUserDetails;
 import org.nrg.xdat.services.XdatUserAuthService;
+import org.nrg.xft.XFTTable;
+import org.nrg.xft.db.PoolDBUtils;
 import org.nrg.xnat.security.tokens.XnatDatabaseUsernamePasswordAuthenticationToken;
-import org.springframework.security.authentication.AuthenticationServiceException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.*;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.authentication.encoding.PlaintextPasswordEncoder;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsChecker;
 
 public class XnatDatabaseAuthenticationProvider extends DaoAuthenticationProvider implements XnatAuthenticationProvider {
+
+    public XnatDatabaseAuthenticationProvider() {
+        super();
+        this.setPreAuthenticationChecks(new PreAuthenticationChecks());
+    }
 
     /**
      * Indicates whether the provider should be visible to and selectable by users. <b>false</b> usually indicates an
@@ -82,5 +89,58 @@ public class XnatDatabaseAuthenticationProvider extends DaoAuthenticationProvide
 
     private String displayName = "";
     private String _providerId = "";
-    private Class plainText = new PlaintextPasswordEncoder().getClass();
+    private Class plainText = PlaintextPasswordEncoder.class;
+
+    private class PreAuthenticationChecks implements UserDetailsChecker {
+        public void check(UserDetails user) {
+            if (!user.isAccountNonLocked()) {
+                logger.debug("User account is locked");
+
+                throw new LockedException(messages.getMessage("AbstractUserDetailsAuthenticationProvider.locked",
+                        "User account is locked"), user);
+            }
+
+            if (!user.isEnabled()) {
+                if (disabledDueToInactivity(user)) {
+                    throw new AccountExpiredException("Attempted login to disabled account: " + user.getUsername(), this);
+                }
+                else {
+                    logger.debug("User account is disabled");
+
+                    throw new DisabledException(messages.getMessage("AbstractUserDetailsAuthenticationProvider.disabled",
+                            "User is disabled"), user);
+                }
+            }
+
+            if (!user.isAccountNonExpired()) {
+                logger.debug("User account is expired");
+
+                throw new AccountExpiredException(messages.getMessage("AbstractUserDetailsAuthenticationProvider.expired",
+                        "User account has expired"), user);
+            }
+        }
+
+        private boolean disabledDueToInactivity(UserDetails user) {
+            try {
+                XDATUserDetails xdatUserDetails = (XDATUserDetails) user;
+                if (!xdatUserDetails.isEnabled()) {
+                    String query = "SELECT COUNT(*) AS count " +
+                            "FROM xdat_user_history " +
+                            "WHERE xdat_user_id=" + xdatUserDetails.getXdatUserId() + " " +
+                            "AND change_user=" + xdatUserDetails.getXdatUserId() + " " +
+                            "AND change_date = (SELECT MAX(change_date) " +
+                            "FROM xdat_user_history " +
+                            "WHERE xdat_user_id=" + xdatUserDetails.getXdatUserId() + " " +
+                            "AND enabled=1)";
+                    Long result = (Long) PoolDBUtils.ReturnStatisticQuery(query, "count", xdatUserDetails.getDBName(), xdatUserDetails.getUsername());
+
+                    return result > 0;
+                }
+            } catch (Exception e) {
+                logger.error(e.getMessage());
+            }
+
+            return false;
+        }
+    }
 }

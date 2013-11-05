1845e8b868c6f8f98f6af84cbc700ad72a8c329f
Mike McKay <mckaym@mir.wustl.edu>
2013-11-05 16:03:31 -0600

    XNAT-2506 Fixed search bugs.
    Former-commit-id: 36704bcee899717a1094b3da998893d86eff3142

--- plugin-resources/webapp/xnat/java/org/nrg/xnat/turbine/modules/actions/QuickSearchAction.java
+++ plugin-resources/webapp/xnat/java/org/nrg/xnat/turbine/modules/actions/QuickSearchAction.java
@@ -36,6 +36,10 @@ import org.nrg.xft.utils.StringUtils;
 
 import java.util.ArrayList;
 import java.util.Hashtable;
+import java.util.Collections;
+import java.util.List;
+
+import com.google.common.collect.Lists;
 
 /**
  * @author Tim
@@ -78,6 +82,7 @@ public class QuickSearchAction extends SecureAction {
             try {
             	String projectQ="SELECT DISTINCT id FROM xnat_projectData prj LEFT JOIN xnat_projectdata_alias ali ON prj.id=ali.aliases_alias_xnat_projectdata_id WHERE lower(id)='" + s +"' OR  lower(secondary_id)='" + s +"' OR  lower(name)='" + s +"' OR  lower(alias)='" + s +"' OR lower(keywords) LIKE '%" + s +"%'";
             	XFTTable table =TableSearch.Execute(projectQ,TurbineUtils.getUser(data).getDBName(),TurbineUtils.getUser(data).getLogin());
+                secureTable(user, table, new String[]{"id"}, "xnat:projectData/ID");
             	if(table.size()>0){
             		if(table.size()==1){
                 		Hashtable row = table.rowHashs().get(0);
@@ -115,6 +120,7 @@ public class QuickSearchAction extends SecureAction {
                 String query = "SELECT DISTINCT s.ID, 'xnat:subjectData' AS element_name, ids,s.project AS project,projects,s.label FROM xnat_subjectData s LEFT JOIN xnat_subjectdata_addid addid ON s.id=addid.xnat_subjectdata_id LEFT JOIN xnat_projectparticipant pp ON s.id=pp.subject_id LEFT JOIN (SELECT project_group.subject_id AS id, btrim(xs_a_concat(project_group.ids || ', '::text), ', '::text) AS ids, btrim(xs_a_concat(('<'::text || project_group.projects) || '>, '::text), ', '::text) AS projects  FROM ( SELECT ((COALESCE(xnat_projectparticipant.label, xnat_projectparticipant.subject_id)::text || ' ('::text) || btrim(xs_a_concat(xnat_projectparticipant.project::text || ', '::text), ', '::text)) || ')'::text AS ids, btrim(xs_a_concat(('<'::text || xnat_projectparticipant.project::text) || '>, '::text), ', '::text) AS projects, xnat_projectparticipant.subject_id FROM xnat_projectparticipant GROUP BY COALESCE(xnat_projectparticipant.label, xnat_projectparticipant.subject_id), xnat_projectparticipant.subject_id) project_group GROUP BY project_group.subject_id) xnat_projs ON s.id=xnat_projs.id WHERE LOWER(s.ID) = '"+ s +"' OR  LOWER(addid) = '"+ s +"' OR LOWER(s.label) = '"+ s +"' OR LOWER(pp.label) = '"+ s +"' UNION SELECT DISTINCT expt.ID,element_name,ids,expt.project,projects,expt.label FROM xnat_experimentData expt LEFT JOIN xnat_experimentData_share proj ON expt.id=proj.sharing_share_xnat_experimentda_id LEFT JOIN xdat_meta_element me ON expt.extension=me.xdat_meta_element_id LEFT JOIN ( SELECT project_group.sharing_share_xnat_experimentda_id AS id, btrim(xs_a_concat(project_group.ids || ', '::text), ', '::text) AS ids, btrim(xs_a_concat(project_group.projects || ', '::text), ', '::text) AS projects   FROM ( SELECT ((COALESCE(proj.label, sharing_share_xnat_experimentda_id)::text || ' ('::text) || btrim(xs_a_concat(proj.project::text || ', '::text), ', '::text)) || ')'::text AS ids, btrim(xs_a_concat(('<'::text || proj.project::text) || '>, '::text), ','::text) AS projects, sharing_share_xnat_experimentda_id           FROM xnat_experimentdata_share proj      LEFT JOIN xnat_experimentdata expt ON proj.sharing_share_xnat_experimentda_id::text = expt.id::text  GROUP BY COALESCE(proj.label, sharing_share_xnat_experimentda_id), sharing_share_xnat_experimentda_id) project_group  GROUP BY project_group.sharing_share_xnat_experimentda_id) xnat_projects ON expt.id=xnat_projects.id WHERE LOWER(expt.ID) = '"+ s +"' OR LOWER(expt.label) = '"+ s +"' OR LOWER(proj.label) = '"+ s +"' ORDER BY element_name;";
                 
                 table =TableSearch.Execute(query,TurbineUtils.getUser(data).getDBName(),TurbineUtils.getUser(data).getLogin());
+                secureTable(user, table, new String[]{"project","projects"}, "xnat:subjectData/project");
 
                 if (table.size()>0)
                 {
@@ -135,7 +141,7 @@ public class QuickSearchAction extends SecureAction {
                     query = "SELECT DISTINCT s.ID, 'xnat:subjectData' AS element_name, ids,s.project AS project,projects,s.label FROM xnat_subjectData s LEFT JOIN xnat_subjectdata_addid addid ON s.id=addid.xnat_subjectdata_id LEFT JOIN xnat_projectparticipant pp ON s.id=pp.subject_id LEFT JOIN (SELECT project_group.subject_id AS id, btrim(xs_a_concat(project_group.ids || ', '::text), ', '::text) AS ids, btrim(xs_a_concat(('<'::text || project_group.projects) || '>, '::text), ', '::text) AS projects  FROM ( SELECT ((COALESCE(xnat_projectparticipant.label, xnat_projectparticipant.subject_id)::text || ' ('::text) || btrim(xs_a_concat(xnat_projectparticipant.project::text || ', '::text), ', '::text)) || ')'::text AS ids, btrim(xs_a_concat(('<'::text || xnat_projectparticipant.project::text) || '>, '::text), ', '::text) AS projects, xnat_projectparticipant.subject_id FROM xnat_projectparticipant GROUP BY COALESCE(xnat_projectparticipant.label, xnat_projectparticipant.subject_id), xnat_projectparticipant.subject_id) project_group GROUP BY project_group.subject_id) xnat_projs ON s.id=xnat_projs.id WHERE LOWER(s.ID) LIKE '%"+ s +"%' OR  LOWER(addid) LIKE '%"+ s +"%' OR LOWER(s.label) LIKE '%"+ s +"%' OR LOWER(pp.label) LIKE '%"+ s +"%' UNION SELECT DISTINCT expt.ID,element_name,ids,expt.project,projects,expt.label FROM xnat_experimentData expt LEFT JOIN xnat_experimentData_share proj ON expt.id=proj.sharing_share_xnat_experimentda_id LEFT JOIN xdat_meta_element me ON expt.extension=me.xdat_meta_element_id LEFT JOIN ( SELECT project_group.sharing_share_xnat_experimentda_id AS id, btrim(xs_a_concat(project_group.ids || ', '::text), ', '::text) AS ids, btrim(xs_a_concat(project_group.projects || ', '::text), ', '::text) AS projects   FROM ( SELECT ((COALESCE(proj.label, sharing_share_xnat_experimentda_id)::text || ' ('::text) || btrim(xs_a_concat(proj.project::text || ', '::text), ', '::text)) || ')'::text AS ids, btrim(xs_a_concat(('<'::text || proj.project::text) || '>, '::text), ','::text) AS projects, sharing_share_xnat_experimentda_id           FROM xnat_experimentdata_share proj      LEFT JOIN xnat_experimentdata expt ON proj.sharing_share_xnat_experimentda_id::text = expt.id::text  GROUP BY COALESCE(proj.label, sharing_share_xnat_experimentda_id), sharing_share_xnat_experimentda_id) project_group  GROUP BY project_group.sharing_share_xnat_experimentda_id) xnat_projects ON expt.id=xnat_projects.id WHERE LOWER(expt.ID) LIKE '%"+ s +"%' OR LOWER(expt.label) LIKE '%"+ s +"%' OR LOWER(proj.label) LIKE '%"+ s +"%' ORDER BY element_name;";
                     
                     table =TableSearch.Execute(query,TurbineUtils.getUser(data).getDBName(),TurbineUtils.getUser(data).getLogin());
-                    
+                    secureTable(user, table, new String[]{"project","projects"}, "xnat:subjectData/project");
                 }
                 
                 int totalCount = 0;
@@ -229,16 +235,18 @@ public class QuickSearchAction extends SecureAction {
                                     }
                                     if (hash.get(element_name).get(p)==null){
                                         hash.get(element_name).put(p,new ArrayList<ItemI>());
+                                        totalCount++;
                                     }
                                     hash.get(element_name).get(p).add(BaseElement.GetGeneratedItem(i));
                                 }
 
                                 if (hash.get(element_name).get(project)==null){
                                     hash.get(element_name).put(project,new ArrayList<ItemI>());
+                                    totalCount++;
                                 }
                                 hash.get(element_name).get(project).add(BaseElement.GetGeneratedItem(i));
-                                
-                                totalCount++;
+
+
                             }
                         }
 
@@ -246,7 +254,13 @@ public class QuickSearchAction extends SecureAction {
                         
                         context.put("matches", hash);
                         context.put("elementNames", elementNames);
-                        context.put("matchCount", totalCount);
+                        int displayCount = 0;
+                        for(Hashtable projects : hash.values()){
+                            for(Object items : projects.values()){
+                                displayCount += ((ArrayList)items).size();
+                            }
+                        }
+                        context.put("matchCount", displayCount);
                         context.put("typeProjectMapping", typeProjectMapping);
                         context.put("pMappings", pMappings);
                         data.setScreenTemplate("QuickSearchMatch.vm");
@@ -269,8 +283,65 @@ public class QuickSearchAction extends SecureAction {
             }
         }
     }
-    
-    
+
+    /**
+     * Remove rows that this user can't see
+     * @param user
+     * @param t
+     * @param projColumns
+     * @param xsiPath
+     */
+    private void secureTable(XDATUser user, XFTTable t, String[] projColumns, String xsiPath) {
+        List<Integer> toRemove=Lists.newArrayList();
+
+        try {
+            if(t.size()>0){
+                t.resetRowCursor();
+
+                List<Integer> columns=Lists.newArrayList();
+                for(String projColumn: projColumns){
+                    Integer pC=t.getColumnIndex(projColumn);
+                    if(pC!=null){
+                        columns.add(pC);
+                    }
+                }
+
+                for(int rowC=0;rowC<t.rows().size();rowC++){
+                    boolean canRead=false;
+                    for(Integer pC: columns){
+                        String pId=(String)t.rows().get(rowC)[pC];
+                        if(!StringUtils.IsEmpty(pId)){
+                            String[] projects=pId.split(", ");
+                            for(String project:projects){
+                                if(!StringUtils.IsEmpty(project)){
+                                    project=project.replace(">", "");
+                                    project=project.replace("<", "");
+                                    if(user.canRead(xsiPath,project)){
+                                        canRead=true;
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                    if(!canRead){
+                        toRemove.add(rowC);
+                    }
+                }
+
+                Collections.reverse(toRemove);
+
+                for(Integer i:toRemove){
+                    t.removeRow(i);
+                }
+
+            }
+        } catch (Exception e) {
+            logger.error("",e);
+        }
+    }
+
     public void doQuickview(RunData data,Context context)
     {
        

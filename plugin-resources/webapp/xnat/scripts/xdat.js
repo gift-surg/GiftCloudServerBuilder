ac0dc45bda06b680eacdae7a6833b10701838c3d
James <james@deck5consulting.com>
2013-12-19 13:14:27 -0600

    Fixes XNAT-2344: Added isDate() function to the proper xdat.js file
    Former-commit-id: 01cb92f44c8072cbb1b8759ab57329c887bf9b35

--- plugin-resources/webapp/xnat/scripts/xdat.js
+++ plugin-resources/webapp/xnat/scripts/xdat.js
@@ -265,6 +265,49 @@ function DaysArray(n) {
    return this
 }
 
+function isDate(dtStr){
+	if (dtStr=="")
+	{
+		return true;
+	}
+	var daysInMonth = DaysArray(12);
+	var pos1=dtStr.indexOf(dtCh);
+	var pos2=dtStr.indexOf(dtCh,pos1+1);
+	var strMonth=dtStr.substring(0,pos1);
+	var strDay=dtStr.substring(pos1+1,pos2);
+	var strYear=dtStr.substring(pos2+1);
+	strYr=strYear;
+	if (strDay.charAt(0)=="0" && strDay.length>1) strDay=strDay.substring(1);
+	if (strMonth.charAt(0)=="0" && strMonth.length>1) strMonth=strMonth.substring(1);
+	for (var i = 1; i <= 3; i++) {
+		if (strYr.charAt(0)=="0" && strYr.length>1) strYr=strYr.substring(1)
+	}
+	month=parseInt(strMonth);
+	day=parseInt(strDay);
+	year=parseInt(strYr);
+	if (pos1==-1 || pos2==-1){
+        xModalMessage('Date Validation', 'The date format should be : mm/dd/yyyy');
+		return false
+	}
+	if (strMonth.length<1 || month<1 || month>12){
+        xModalMessage('Date Validation', 'Please enter a valid month');
+		return false
+	}
+	if (strDay.length<1 || day<1 || day>31 || (month==2 && day>daysInFebruary(year)) || day > daysInMonth[month]){
+        xModalMessage('Date Validation', 'Please enter a valid day');
+		return false
+	}
+	if (strYear.length != 4 || year==0 || year<minYear || year>maxYear){
+        xModalMessage('Date Validation', 'Please enter a valid 4 digit year between '+minYear+' and '+maxYear);
+		return false
+	}
+	if (dtStr.indexOf(dtCh,pos2+1)!=-1 || isInteger(stripCharsInBag(dtStr, dtCh))==false){
+        xModalMessage('Date Validation', 'Please enter a valid date');
+		return false
+	}
+    return true
+}
+
 function concealContent(message){
 	if(!message) {
 		message = "Submitting... Please wait.";

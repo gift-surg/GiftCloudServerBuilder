cfbf669cb28206c259de52a4659e353ff052d004
Rick Herrick <rick.herrick@wustl.edu>
2014-02-10 15:22:09 -0600

    XNAT-2347: Problem occurred with scans that have no type specified. These are passed as NULL to the DownloadSessionsAction2 and have to be handled specially in the generated SQL query. Fix detects both populated and unpopulated (NULL) scan types and composes the query appropriately.
    Former-commit-id: ccada4305ddbdd32fa795dbe390e26940d3195a0

--- plugin-resources/webapp/xnat/java/org/nrg/xnat/turbine/modules/actions/DownloadSessionsAction2.java
+++ plugin-resources/webapp/xnat/java/org/nrg/xnat/turbine/modules/actions/DownloadSessionsAction2.java
@@ -10,6 +10,7 @@
  */
 package org.nrg.xnat.turbine.modules.actions;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.turbine.util.RunData;
 import org.apache.velocity.context.Context;
 import org.nrg.xdat.bean.CatCatalogBean;
@@ -31,7 +32,7 @@ public class DownloadSessionsAction2 extends SecureAction {
     /* (non-Javadoc)
      * @see org.apache.turbine.modules.actions.VelocitySecureAction#doPerform(org.apache.turbine.util.RunData, org.apache.velocity.context.Context)
      */
-    @SuppressWarnings("ConstantConditions")
+    @SuppressWarnings({"ConstantConditions", "unchecked"})
     @Override
     public void doPerform(RunData data, Context context) throws Exception {
         String [] session_ids=((String[])TurbineUtils.GetPassedObjects("sessions",data));
@@ -73,7 +74,7 @@ public class DownloadSessionsAction2 extends SecureAction {
 
             // narrow down the range of scan types to only the ones relevant to this session
             if (requestScanTypes != null && requestScanTypes.length > 0) {
-                String query = "SELECT id FROM xnat_imagescandata WHERE image_session_id = '" + session + "' AND type IN (" + sqlList(requestScanTypes) + ")";
+                String query = "SELECT id FROM xnat_imagescandata WHERE image_session_id = '" + session + "' AND " + getTypeClause(requestScanTypes);
                 XFTTable table = XFTTable.Execute(query, TurbineUtils.getUser(data).getDBName(), TurbineUtils.getUser(data).getLogin());
                 List<String> sessionScans = table.convertColumnToArrayList("id");
                 if (sessionScans != null && sessionScans.size() > 0) {
@@ -149,9 +150,9 @@ public class DownloadSessionsAction2 extends SecureAction {
         
         String id = Calendar.getInstance().getTimeInMillis() + "";
         File f = TurbineUtils.getUser(data).getCachedFile("catalogs/" + id + ".xml");
-        
-        f.getParentFile().mkdirs();
-        
+
+        boolean mkdirs = f.getParentFile().mkdirs();
+
         FileWriter fw = new FileWriter(f);
         cat.toXML(fw, true);
         fw.flush();
@@ -168,13 +169,34 @@ public class DownloadSessionsAction2 extends SecureAction {
         }     
     }
 
-    private String sqlList(String[] list) {
-        StringBuilder sb = new StringBuilder();
-        for (String item : list) {
-            if (sb.length() > 0) sb.append(',');
-            sb.append("'").append(item).append("'");
+    private String getTypeClause(final String[] requestScanTypes) {
+        StringBuilder buffer = new StringBuilder();
+        boolean foundNull = false;
+        for (String item : requestScanTypes) {
+            if (StringUtils.isBlank(item) || item.equalsIgnoreCase("NULL")) {
+                foundNull = true;
+                continue;
+            }
+            buffer.append("'").append(item).append("', ");
         }
-        return sb.toString();
+        int length = buffer.length();
+        if (length == 0 && !foundNull) {
+            throw new RuntimeException("Bad state found: no scan types specified, not even NULL!");
+        }
+        if (length > 0) {
+            buffer.delete(length - 2, length);
+        }
+        if (foundNull) {
+            if (length > 0) {
+                buffer.insert(0, "(type IN (");
+                buffer.append(") OR type is NULL)");
+            } else {
+                buffer.append("type is NULL");
+            }
+        } else {
+            buffer.insert(0, "type IN (");
+            buffer.append(")");
+        }
+        return buffer.toString();
     }
-
 }

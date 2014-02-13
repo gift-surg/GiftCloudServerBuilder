e638f2b59f8f35ec94c0199e724df6e2da31cc12
Rick Herrick <rherrick@gmail.com>
2014-02-12 21:02:51 -0600

    XNAT-2897: Added ability for admins to get session count for other users.
    Former-commit-id: 6abe29296234c1e6d96968989fc24ddea7b8b4c9

--- plugin-resources/webapp/xnat/java/org/nrg/xnat/restlet/extensions/SessionCountRestlet.java
+++ plugin-resources/webapp/xnat/java/org/nrg/xnat/restlet/extensions/SessionCountRestlet.java
@@ -10,16 +10,21 @@
  */
 package org.nrg.xnat.restlet.extensions;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.nrg.xdat.XDAT;
+import org.nrg.xdat.entities.XDATUserDetails;
+import org.nrg.xdat.om.XdatUser;
 import org.nrg.xdat.security.XDATUser;
+import org.nrg.xft.security.UserI;
 import org.nrg.xnat.restlet.XnatRestlet;
 import org.nrg.xnat.restlet.resources.SecureResource;
 import org.restlet.Context;
 import org.restlet.data.MediaType;
 import org.restlet.data.Request;
 import org.restlet.data.Response;
+import org.restlet.data.Status;
 import org.restlet.resource.Representation;
 import org.restlet.resource.ResourceException;
 import org.restlet.resource.StringRepresentation;
@@ -30,13 +35,48 @@ import org.springframework.security.core.session.SessionRegistryImpl;
 
 import java.util.List;
 
-@XnatRestlet("/services/sessions")
+@XnatRestlet({"/services/sessions", "/services/sessions/{USERNAME}"})
 public class SessionCountRestlet extends SecureResource {
+    public static final String PARAM_USERNAME = "USERNAME";
+
     private static final Log _log = LogFactory.getLog(SessionCountRestlet.class);
+
+    private final UserI _user;
 
     public SessionCountRestlet(Context context, Request request, Response response) {
         super(context, request, response);
         this.getVariants().add(new Variant(MediaType.ALL));
+
+        final String username = (String) getRequest().getAttributes().get(PARAM_USERNAME);
+
+        // You can't request another user's session count unless you're a site admin.
+        if (!StringUtils.isBlank(username)) {
+            // But if it's just you, no harm no foul.
+            if (username.equals(user.getLogin())) {
+                _user = user;
+            } else if (!user.isSiteAdmin()) {
+                // If it's NOT you and you're not an admin, you are banished.
+                getResponse().setStatus(Status.CLIENT_ERROR_FORBIDDEN, "Only site admins can request the session count for another user.");
+                _user = null;
+            } else {
+                // If you are an admin and this isn't you, then let's get that account.
+                final XdatUser xdatUser = XDATUser.getXdatUsersByLogin(username, user, false);
+                if (xdatUser == null) {
+                    getResponse().setStatus(Status.CLIENT_ERROR_BAD_REQUEST, "The user identified by " + username + " can not be found in the system.");
+                    _user = null;
+                } else {
+                    XDATUserDetails temp = null;
+                    try {
+                        temp = new XDATUserDetails(xdatUser);
+                    } catch (Exception e) {
+                        getResponse().setStatus(Status.SERVER_ERROR_INTERNAL, "Something went wrong retrieving the user " + username);
+                    }
+                    _user = temp;
+                }
+            }
+        } else {
+            _user = user;
+        }
     }
 
     @Override
@@ -45,18 +85,18 @@ public class SessionCountRestlet extends SecureResource {
             _log.debug("Entering the session count represent() method");
         }
 
-        int sessionCount = getSessionCount(user);
+        int sessionCount = getSessionCount(_user);
         return new StringRepresentation(Integer.toString(sessionCount));
     }
 
-    private int getSessionCount(XDATUser user) {
+    private int getSessionCount(UserI user) {
         SessionRegistry sessionRegistry = XDAT.getContextService().getBean("sessionRegistry", SessionRegistryImpl.class);
 
         int sessionCount = 0;
 
-        if(sessionRegistry != null){
+        if (sessionRegistry != null) {
             List<SessionInformation> l = sessionRegistry.getAllSessions(user, false);
-            if(l != null){
+            if (l != null) {
                 sessionCount = l.size();
             }
         }

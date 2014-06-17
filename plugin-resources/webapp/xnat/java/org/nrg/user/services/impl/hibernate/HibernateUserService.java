5b9536c34cf07bf91bb1df86df0017e18a5b317b
Rick Herrick <rick.herrick@wustl.edu>
2014-06-17 14:49:16 -0500

    Added DataCacheService and support classes. Updated unit tests. Adapted to new library pattern, generized DAO, removed newEntity() override.
    Former-commit-id: 3b1da61225ebe4117a13e9a3e0da54975120f2cc

--- plugin-resources/webapp/xnat/java/org/nrg/user/services/impl/hibernate/HibernateUserService.java
+++ plugin-resources/webapp/xnat/java/org/nrg/user/services/impl/hibernate/HibernateUserService.java
@@ -14,12 +14,11 @@ import org.nrg.framework.orm.hibernate.AbstractHibernateEntityService;
 import org.nrg.user.daos.UserDAO;
 import org.nrg.user.entities.User;
 import org.nrg.user.services.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-public class HibernateUserService extends AbstractHibernateEntityService<User> implements UserService {
+public class HibernateUserService extends AbstractHibernateEntityService<User, UserDAO> implements UserService {
 
     /**
      * @see org.nrg.user.services.UserService#getUserByName(java.lang.String)
@@ -27,25 +26,6 @@ public class HibernateUserService extends AbstractHibernateEntityService<User> i
     @Override
     @Transactional
     public User getUserByName(String name) {
-        return _dao.getUserByName(name);
+        return getDao().getUserByName(name);
     }
-
-    /**
-     * @see org.nrg.framework.orm.hibernate.AbstractHibernateEntityService#newEntity()
-     */
-    @Override
-    public User newEntity() {
-        return new User();
-    }
-
-    /**
-     * @see org.nrg.framework.orm.hibernate.AbstractHibernateEntityService#getDao()
-     */
-    @Override
-    protected UserDAO getDao() {
-        return _dao;
-    }
-
-    @Autowired
-    private UserDAO _dao;
 }

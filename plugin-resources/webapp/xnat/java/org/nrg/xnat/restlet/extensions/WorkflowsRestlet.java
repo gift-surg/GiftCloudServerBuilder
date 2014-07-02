31359eb323edd81f6100a627243395b337afaf29
Rick Herrick <rick.herrick@wustl.edu>
2014-07-02 09:57:12 -0500

    XNAT-3183: Added logging and better exception handling to try to generate messaging to understand where cycles are going when CNDA locks up on deleting pipelines from the site.
    Former-commit-id: 6eabb402ae15f8cc871139655d952273d9b09054

--- plugin-resources/webapp/xnat/java/org/nrg/xnat/restlet/extensions/WorkflowsRestlet.java
+++ plugin-resources/webapp/xnat/java/org/nrg/xnat/restlet/extensions/WorkflowsRestlet.java
@@ -10,11 +10,9 @@
  */
 package org.nrg.xnat.restlet.extensions;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import org.apache.commons.lang.StringUtils;
 import org.nrg.xdat.om.WrkWorkflowdata;
 import org.nrg.xdat.om.XnatExperimentdata;
-import org.nrg.xdat.om.XnatSubjectassessordata;
 import org.nrg.xdat.turbine.utils.PopulateItem;
 import org.nrg.xft.XFTItem;
 import org.nrg.xft.XFTTable;
@@ -30,6 +28,8 @@ import org.restlet.data.Response;
 import org.restlet.data.Status;
 import org.restlet.resource.Representation;
 import org.restlet.resource.Variant;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.Hashtable;
@@ -37,7 +37,7 @@ import java.util.Map;
 
 @XnatRestlet({"/services/workflows","/services/workflows/{PIPELINE_NAME}","/services/workflows/workflowid/{WORKFLOW_PRIMARY_KEY}"})
 public class WorkflowsRestlet extends SecureResource {
-    private static final Log _log = LogFactory.getLog(WorkflowsRestlet.class);
+    private static final Logger _log = LoggerFactory.getLogger(WorkflowsRestlet.class);
 
 	String pipeline_name = null;
 	String workflow_primary_key=null;
@@ -45,7 +45,6 @@ public class WorkflowsRestlet extends SecureResource {
 	String xnat_id = null;
 	String status = null;
 	final static String DISPLAY_LATEST = "LATEST";
-	final static String DISPLAY_ALL = "ALL";
 	String display = DISPLAY_LATEST;
 	Map<String,Object> form_params = null;
 	String[] pipelineList = null;
@@ -60,20 +59,24 @@ public class WorkflowsRestlet extends SecureResource {
 		form_params = this.getQueryVariablesAsMap();
 		xnat_id = this.getQueryVariable("experiment");
 		latest_by_param = this.getQueryVariable("latest_by_param");
-		if (pipeline_name != null)
+
+		if (StringUtils.isNotBlank(pipeline_name))
 			pipelineList = pipeline_name.split(",");
-		if (display == null) 
+		if (StringUtils.isBlank(display)) {
 			display = DISPLAY_LATEST;
-		if (workflow_primary_key !=null) {
+        }
+		if (StringUtils.isNotBlank(workflow_primary_key)) {
 			this.getVariants().add(new Variant(MediaType.APPLICATION_ALL_XML));			
 		}else {
 			this.getVariants().add(new Variant(MediaType.APPLICATION_EXCEL));
 		}
-		System.out.println("WorkflowRestlet invoked " + status + " " + pipeline_name + " " + xnat_id + " " + project_id);
+		if (_log.isInfoEnabled()) {
+            _log.info(getWorkflowParameters());
+        }
 	}
 
 	@Override
-	public Representation getRepresentation(Variant variant) {
+	public Representation represent(Variant variant) {
 		MediaType mt = overrideVariant(variant);
 		if (workflow_primary_key !=null) {
 			PersistentWorkflowI wrkFlow = PersistentWorkflowUtils.getWorkflowByEventId(user,Integer.parseInt(workflow_primary_key));
@@ -84,7 +87,7 @@ public class WorkflowsRestlet extends SecureResource {
 				expt=XnatExperimentdata.getXnatExperimentdatasById(xnat_id, user, false);
 				if(project_id!=null){
 					if(expt==null){
-						expt=(XnatSubjectassessordata)XnatExperimentdata.GetExptByProjectIdentifier(project_id, xnat_id,user, false);
+						expt = XnatExperimentdata.GetExptByProjectIdentifier(project_id, xnat_id,user, false);
 					}
 				}
 			}
@@ -117,11 +120,12 @@ public class WorkflowsRestlet extends SecureResource {
 					   XFTTable table=XFTTable.Execute(query, user.getDBName(), userName);
 					   if (table.size()==1 && table.hasMoreRows()) {
 						   ArrayList<Hashtable> tableToList = table.toArrayListOfHashtables();
-						   Hashtable rowHash = tableToList.get(0);
+                           @SuppressWarnings("unchecked")
+                           Hashtable<String, Object> rowHash = tableToList.get(0);
 						   PopulateItem populator = PopulateItem.Populate(rowHash, user, "wrk:workflowdata", true);
 						   return representItem(populator.getItem(), mt);
 					   }else {
-						   Hashtable params = new Hashtable();
+						   Hashtable<String, Object> params = new Hashtable<String, Object>();
 						   return representTable(table, mt, params);
 					   }
 				   }else {
@@ -145,9 +149,9 @@ public class WorkflowsRestlet extends SecureResource {
 			            }
 				   }
 		       }catch(Exception e) {
-				   _log.error(e);
-					this.getResponse().setStatus(Status.CLIENT_ERROR_NOT_FOUND,
-					"Unable to find workflow entry for pipeline " + pipeline_name + " and experiment." + expt.getId());
+                   final String message = "Unable to find workflow entry for pipeline " + pipeline_name + " and experiment." + expt.getId();
+                   _log.error(message, e);
+                   getResponse().setStatus(Status.CLIENT_ERROR_NOT_FOUND, message);
 					return null;
 			   }
 			}else {
@@ -266,19 +270,32 @@ public class WorkflowsRestlet extends SecureResource {
 		return false;
 	}
 
-
-	private void addWorkflowParamsConstraints(CriteriaCollection cc) {
-		if (form_params.size()>0) {
-		for(String s:form_params.keySet()){
-			if (s.startsWith("param_")) {
-				String wrkflow_paramName = s.substring(6);
-				String wrkflow_paramValue = (String)form_params.get(s);
-				cc.addClause("wrk:workflowData.executionEnvironment.parameters.parameter.name", wrkflow_paramName);
-				cc.addClause("wrk:workflowData.executionEnvironment.parameters.parameter",wrkflow_paramValue);
-			}
+    private String getWorkflowParameters() {
+        return "WorkflowRestlet invoked with parameters:\r" +
+                formatParameter("Pipeline name", pipeline_name) +
+                formatParameter("Project ID", project_id) +
+                formatParameter("Experiment ID", xnat_id) +
+                formatParameter("Workflow ID", workflow_primary_key) +
+                formatParameter("Status", status) +
+                formatParameter("Display", display) +
+                formatParameter("Latest by param", latest_by_param) +
+                formatParameter("Form parameters", form_params);
 		}
-		}
 
+    private String formatParameter(final String name, final Object value) {
+        final String formattedName = StringUtils.rightPad(name, 20);
+        if (value instanceof String && StringUtils.isNotBlank((String) value)) {
+            return String.format(" * %s: %s\r", formattedName, value);
+        } else if (value instanceof Map) {
+            @SuppressWarnings("unchecked")
+            final Map<String, Object> map = (Map<String, Object>) value;
+            final StringBuilder buffer = new StringBuilder(" * ").append(formattedName).append(":\r");
+            for (final String key : map.keySet()) {
+                buffer.append("    - ").append(String.format("%s: %s\r", StringUtils.rightPad(key, 20), map.get(key).toString())).append("\r");
+            }
+            return buffer.toString();
+        }
+        return "";
 	}
 
 	private String addWorkflowParamsConstraints() {

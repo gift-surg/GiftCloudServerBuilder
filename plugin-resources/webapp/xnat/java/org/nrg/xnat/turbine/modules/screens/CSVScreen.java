fe7431dc3d4676fac808a644cfd4bb6389d01469
Rick Herrick <rick.herrick@wustl.edu>
2014-08-04 16:55:39 -0500

    XNAT-3029: Moved CSV formatting to use StringEscapeUtils.escapeCsv(). This quotes the string where necessary according to CSV guidelines, especially for Excel.
    Changes to xdat jars are from changeset https://bitbucket.org/nrg/xdat_1_6dev/commits/9ff1afeda9b86d8750450ee256e031ce15ee017e.
    
    
    Former-commit-id: 7f94981f70c30c4cf04a8696a71935ed35e4d401

--- plugin-resources/webapp/xnat/java/org/nrg/xnat/turbine/modules/screens/CSVScreen.java
+++ plugin-resources/webapp/xnat/java/org/nrg/xnat/turbine/modules/screens/CSVScreen.java
@@ -12,11 +12,11 @@ package org.nrg.xnat.turbine.modules.screens;
 
 import org.apache.turbine.util.RunData;
 
+@SuppressWarnings("unused")
 public class CSVScreen extends org.nrg.xdat.turbine.modules.screens.CSVScreen {
 
 	@Override
 	public String getContentType(RunData data) {
 		return "text/csv";
 	}
-
 }

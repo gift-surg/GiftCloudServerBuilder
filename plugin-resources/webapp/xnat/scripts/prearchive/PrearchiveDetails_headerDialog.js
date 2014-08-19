77584464680da9bc72bad4918731cd8066623ce0
Joel Balestra <joelbalestra@gmail.com>
2014-08-19 10:38:09 -0700

    Replaced YUI modal with xModal and using jquery for ajax call.
    Former-commit-id: b05a6cfc27791b8df7693643f554898396389afd

--- plugin-resources/webapp/xnat/scripts/prearchive/PrearchiveDetails_headerDialog.js
+++ plugin-resources/webapp/xnat/scripts/prearchive/PrearchiveDetails_headerDialog.js
@@ -1,44 +1,34 @@
-//Author: Tim Olsen <tim@deck5consulting.com>
+XNAT.app.headerDialog = XNAT.app.headerDialog || {};
 
-XNAT.app.headerDialog={
-	loaded:false,
-	ready:false,
-	show:function(url){
-		//if its initialized, then show it, otherwise initialize it.
-		if(!this.loaded){
-			this.loaded=true;
-			this.start(url);
-		}else if(this.ready){
-			XNAT.app.headerDialog.dialog.render(document.body);
-			XNAT.app.headerDialog.dialog.show();
-		}
-	},
-	load:function(url,title){
-		this.loaded=false;
-		this.ready=false;
-		this.dialog.hide();
-		YUIDOM.get('headerDialog_content').innerHTML="";
-		YUIDOM.get('headerDialog_header').innerHTML=title;
-		this.show(url);
-	},
-	start:function(url){		  		
-		//triggers the initial load of the resource list for the select box
-		openModalPanel("resource_loading","Loading");
-		YAHOO.util.Connect.asyncRequest('GET', url, {success : this.handleLoad, failure : function(){
-			closeModalPanel("resource_loading");
-		}, cache : false, scope : this});
-	},
-	handleLoad:function(obj){
-		//handles the response with the available resources for this project
-		closeModalPanel("resource_loading");
-		YUIDOM.get('headerDialog_content').innerHTML=obj.responseText;
+XNAT.app.headerDialog.load = function( url, title ){
 
-		this.ready=true;
-		this.show();
-	}
-}
+    openModalPanel("resource_loading","Loading");
 
+    var modalOpts={};
+    modalOpts.width = 700;
+    modalOpts.height = 500;
+    modalOpts.okLabel = 'Close';
+    modalOpts.cancel = 'hide';
 
-//initialize modal upload dialog
-XNAT.app.headerDialog.dialog=new YAHOO.widget.Dialog("header_dialog", { fixedcenter:true, visible:false, width:"700px", height:"500px", modal:true, close:true, draggable:true } ),
-XNAT.app.headerDialog.dialog.cfg.queueProperty("buttons", [{ text:"Close", handler:{fn:function(){XNAT.app.headerDialog.dialog.hide();}}, isDefault:true}]);
+    var getData = $.ajax({
+        type: 'GET',
+        url: url,
+        cache: false ,
+        dataType: 'html'
+    });
+
+    getData.done(function(data){
+        modalOpts.title = title;
+        modalOpts.content = data;
+        xModalOpenNew(modalOpts);
+        closeModalPanel("resource_loading");
+    });
+
+    getData.fail(function(jqXHR, textStatus, errorThrown){
+        modalOpts.title = 'Error - ' + title;
+        modalOpts.content = 'Error: ' + textStatus;
+        xModalOpenNew(modalOpts);
+        closeModalPanel("resource_loading");
+    });
+
+};

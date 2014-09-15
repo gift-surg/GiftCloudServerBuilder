dc26dcc8e66d286fb605cf7984a6e680f0300c4d
Rick Herrick <rick.herrick@wustl.edu>
2014-09-15 11:02:06 -0500

    XNAT-3372 Added check for null project and returns HTTP 404 when that occurs.
    Former-commit-id: aaae7202bf2b10d4ab73f5f1bd8e865f8e4aa73c

--- plugin-resources/webapp/xnat/java/org/nrg/xnat/restlet/resources/ProjectUserListResource.java
+++ plugin-resources/webapp/xnat/java/org/nrg/xnat/restlet/resources/ProjectUserListResource.java
@@ -37,26 +37,30 @@ public class ProjectUserListResource extends SecureResource {
     public ProjectUserListResource(Context context, Request request, Response response) {
         super(context, request, response);
 
-        this.getVariants().add(new Variant(MediaType.APPLICATION_JSON));
-        this.getVariants().add(new Variant(MediaType.TEXT_HTML));
-        this.getVariants().add(new Variant(MediaType.TEXT_XML));
+        getVariants().add(new Variant(MediaType.APPLICATION_JSON));
+        getVariants().add(new Variant(MediaType.TEXT_HTML));
+        getVariants().add(new Variant(MediaType.TEXT_XML));
 
         String pID = (String) getParameter(request, "PROJECT_ID");
         if (pID != null) {
             proj = XnatProjectdata.getProjectByIDorAlias(pID, user, false);
         }
+        if (proj == null) {
+            getResponse().setStatus(Status.CLIENT_ERROR_NOT_FOUND, "The project ID " + pID + " does not appear to be a valid project ID. Please verify your information.");
+        } else {
         final Object projectData = proj.getItem().getProps().get("projectdata_info");
         if (!(projectData instanceof Integer)) {
             String message = "Can't parse the project data info identifier property for project " + proj.getDisplayName() + ". Object is " + (projectData == null ? "null" : projectData.getClass().getName()) + ". Must be an Integer object.";
             logger.error(message);
-            this.getResponse().setStatus(Status.CLIENT_ERROR_FORBIDDEN, message);
+                getResponse().setStatus(Status.CLIENT_ERROR_FORBIDDEN, message);
         } else {
             int projectId = (Integer) projectData;
             if (!(user.isSiteAdmin() || user.isOwner(proj.getId()) || isWhitelisted(projectId))) {
             logger.error("Unauthorized Access to project-level user resources. User: " + userName);
-            this.getResponse().setStatus(Status.CLIENT_ERROR_FORBIDDEN, "Access Denied: Only project owners and site managers can access user resources.");
+                    getResponse().setStatus(Status.CLIENT_ERROR_FORBIDDEN, "Access Denied: Only project owners and site managers can access user resources.");
         }
         displayHiddenUsers = Boolean.parseBoolean((String)getParameter(request, "DISPLAY_HIDDEN_USERS"));
+    }
     }
     }
 
@@ -70,7 +74,7 @@ public class ProjectUserListResource extends SecureResource {
 
         if (proj != null) {
             try {
-                StringBuffer query = new StringBuffer("SELECT g.id AS \"GROUP_ID\", displayname,login,firstname,lastname,email FROM xdat_userGroup g RIGHT JOIN xdat_user_Groupid map ON g.id=map.groupid RIGHT JOIN xdat_user u ON map.groups_groupid_xdat_user_xdat_user_id=u.xdat_user_id WHERE tag='").append(proj.getId()).append("' ");
+                StringBuilder query = new StringBuilder("SELECT g.id AS \"GROUP_ID\", displayname,login,firstname,lastname,email FROM xdat_userGroup g RIGHT JOIN xdat_user_Groupid map ON g.id=map.groupid RIGHT JOIN xdat_user u ON map.groups_groupid_xdat_user_xdat_user_id=u.xdat_user_id WHERE tag='").append(proj.getId()).append("' ");
                 if(!displayHiddenUsers){
                     query.append(" and enabled = 1 ");
                 }
@@ -89,7 +93,7 @@ public class ProjectUserListResource extends SecureResource {
         MediaType mt = overrideVariant(variant);
 
         if(table!=null)params.put("totalRecords", table.size());
-        return this.representTable(table, mt, params);
+        return representTable(table, mt, params);
     }
 
     public boolean isWhitelisted() {

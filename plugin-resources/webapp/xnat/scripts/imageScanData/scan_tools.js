35b81d42123f103955117620a2da94a261f24434
Mark M. Florida <mflorida@gmail.com>
2014-09-23 08:27:06 -0500

    Fix for yesterday's fix (I didn't realize sometimes an <input> could be a <select> and a <select> with a 'size' attribute turns it into a select *box*).
    Former-commit-id: 4f9948d2ac50431cf7713bf74740c201b7ebb31d

--- plugin-resources/webapp/xnat/scripts/imageScanData/scan_tools.js
+++ plugin-resources/webapp/xnat/scripts/imageScanData/scan_tools.js
@@ -1089,8 +1089,8 @@ function scanListingEditor(_tbody,_scanSet,_options){
 				}
 			}
 
-            //scan.type_input.style.width="100%";
-            scan.type_input.size="25";
+            scan.type_input.style.width="180px";
+            //scan.type_input.size="25";
             scan.type_input.id=elementName + scanXPath(modality, scanTypeTable) + "/type";
             scan.type_input.name=elementName +scanXPath(modality, scanTypeTable) + "/type";
 

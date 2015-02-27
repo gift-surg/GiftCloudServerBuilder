021b64952e8319f0d78777a7f5ad19c223300cb0
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2015-02-27 13:42:24 +0000

    switched to RSpec
    Former-commit-id: a9dcc9bd4b6ce103582cf73e759b5a4be53b7af4

--- bin/gift-test-system/ruby/test_gift_cloud.rb
+++ bin/gift-test-system/ruby/test_gift_cloud.rb
@@ -1,77 +1,71 @@
-require 'minitest/autorun'
-require 'minitest/reporters'
-require 'shoulda/context'
+require 'rspec'
 
-class TestGiftCloud < Minitest::Test
-  context 'Upload client' do
-    context '(before any operation)' do
-      should 'sign in' do
-        skip 'not implemented'
-      end
-    end
-    
-    context '(after sign-in)' do
-      # PROJECT ==========================================
-      should 'create a new project' do
-        skip 'not implemented'
-      end
-      
-      should 'not re-create an existing project' do
-        skip 'not implemented'
-      end
-      
-      should 'list all projects accessible to user' do
-        skip 'not implemented'
-      end
-      
-      should 'not list any project not accessible to user' do
-        skip 'not implemented'
-      end
-      # ==================================================
-      
-      # SUBJECT ==========================================
-      should 'create a new subject' do
-        skip 'not implemented'
-      end
-      
-      should 'not re-create an existing subject' do
-        skip 'not implemented'
-      end
-      
-      should 'list all subjects in a project' do
-        skip 'not implemented'
-      end
-      # ==================================================
-      
-      # UPLOAD ===========================================
-      should 'upload zipped DICOM studies of a subject to a new session' do
-        skip 'not implemented'
-      end
-      
-      should 'upload zipped DICOM studies of a subject to an existing session' do
-        skip 'not implemented'
-      end
-      # ==================================================
-      
-      # PSEUDONYM ========================================
-      should 'create a new pseudonym for a subject' do
-        skip 'not implemented'
-      end
-      
-      should 'retrieve subject corresponding to an existing pseudonym' do
-        skip 'not implemented'
-      end
-      
-      should 'not re-create an existing pseudonym' do
-        skip 'not implemented'
-      end
-      # ==================================================
-    end
-  
-    context '(after all operations)' do
-      should 'sign out' do
-        skip 'not implemented'
-      end
-    end
+require_relative 'client'
+
+RSpec.describe GiftCloud::Client do
+  # AUTHENTICATION ===================================
+  it 'signs in' do
+    # skip 'not implemented'
   end
+  
+  it 'signs out' do
+    # skip 'not implemented'
+  end
+  # ==================================================
+  
+  # PROJECT ==========================================
+  it 'creates a new project' do
+    # skip 'not implemented'
+  end
+  
+  it 'does not re-create an existing project' do
+    # skip 'not implemented'
+  end
+  
+  it 'lists all projects accessible to user' do
+    # skip 'not implemented'
+  end
+  
+  it 'does not list any project not accessible to user' do
+    # skip 'not implemented'
+  end
+  # ==================================================
+  
+  # SUBJECT ==========================================
+  it 'creates a new subject' do
+    # skip 'not implemented'
+  end
+  
+  it 'does not re-create an existing subject' do
+    # skip 'not implemented'
+  end
+  
+  it 'lists all subjects in a project' do
+    # skip 'not implemented'
+  end
+  # ==================================================
+  
+  # UPLOAD ===========================================
+  it 'uploads zipped DICOM studies of a subject to a new session' do
+    # skip 'not implemented'
+  end
+  
+  it 'uploads zipped DICOM studies of a subject to an existing session' do
+    # skip 'not implemented'
+  end
+  # ==================================================
+  
+  # PSEUDONYM ========================================
+  it 'creates a new pseudonym for a subject' do
+    # skip 'not implemented'
+  end
+  
+  it 'retrieves subject corresponding to an existing pseudonym' do
+    # skip 'not implemented'
+  end
+  
+  it 'does not re-create an existing pseudonym' do
+    # skip 'not implemented'
+  end
+  # ==================================================
 end

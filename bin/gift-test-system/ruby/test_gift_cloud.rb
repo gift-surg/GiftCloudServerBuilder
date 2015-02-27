79fa0ec51c476a7682ea8e139a8ab5e3763ea3d4
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2015-02-27 13:48:08 +0000

    grouping according to entity involved
    Former-commit-id: 8771289fd4fb3ec180430c2165f1a2692e1b8957

--- bin/gift-test-system/ruby/test_gift_cloud.rb
+++ bin/gift-test-system/ruby/test_gift_cloud.rb
@@ -4,68 +4,78 @@ require_relative 'client'
 
 RSpec.describe GiftCloud::Client do
   # AUTHENTICATION ===================================
-  it 'signs in' do
-    skip 'not implemented'
-  end
-  
-  it 'signs out' do
-    skip 'not implemented'
+  describe '(authentication)' do
+    it 'signs in' do
+      skip 'not implemented'
+    end
+    
+    it 'signs out' do
+      skip 'not implemented'
+    end
   end
   # ==================================================
   
   # PROJECT ==========================================
-  it 'creates a new project' do
-    skip 'not implemented'
-  end
-  
-  it 'does not re-create an existing project' do
-    skip 'not implemented'
-  end
-  
-  it 'lists all projects accessible to user' do
-    skip 'not implemented'
-  end
-  
-  it 'does not list any project not accessible to user' do
-    skip 'not implemented'
+  describe '(project)' do
+    it 'creates new' do
+      skip 'not implemented'
+    end
+    
+    it 'does not re-create existing' do
+      skip 'not implemented'
+    end
+    
+    it 'lists all accessible to user' do
+      skip 'not implemented'
+    end
+    
+    it 'does not list any not accessible to user' do
+      skip 'not implemented'
+    end
   end
   # ==================================================
   
   # SUBJECT ==========================================
-  it 'creates a new subject' do
-    skip 'not implemented'
-  end
-  
-  it 'does not re-create an existing subject' do
-    skip 'not implemented'
-  end
-  
-  it 'lists all subjects in a project' do
-    skip 'not implemented'
+  describe '(subject)' do
+    it 'creates new' do
+      skip 'not implemented'
+    end
+    
+    it 'does not re-create existing' do
+      skip 'not implemented'
+    end
+    
+    it 'lists all in a project' do
+      skip 'not implemented'
+    end
   end
   # ==================================================
   
   # UPLOAD ===========================================
-  it 'uploads zipped DICOM studies of a subject to a new session' do
-    skip 'not implemented'
-  end
-  
-  it 'uploads zipped DICOM studies of a subject to an existing session' do
-    skip 'not implemented'
+  describe '(session)' do
+    it 'uploads zipped DICOM studies of a subject to new' do
+      skip 'not implemented'
+    end
+    
+    it 'uploads zipped DICOM studies of a subject to existing' do
+      skip 'not implemented'
+    end
   end
   # ==================================================
   
   # PSEUDONYM ========================================
-  it 'creates a new pseudonym for a subject' do
-    skip 'not implemented'
-  end
-  
-  it 'retrieves subject corresponding to an existing pseudonym' do
-    skip 'not implemented'
-  end
-  
-  it 'does not re-create an existing pseudonym' do
-    skip 'not implemented'
+  describe '(pseudonym)' do
+    it 'creates new for a subject' do
+      skip 'not implemented'
+    end
+    
+    it 'retrieves subject corresponding to existing' do
+      skip 'not implemented'
+    end
+    
+    it 'does not re-create existing' do
+      skip 'not implemented'
+    end
   end
   # ==================================================
 end

4236fa2f11158fc2c4ed1b2922633bacbd69259c
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2015-02-27 13:43:31 +0000

    skipped examples (tests)
    Former-commit-id: 8ebd81f251cc3b3bcb4401cc7e215b5123eadcdb

---
+++ bin/gift-test-system/ruby/client.rb
@@ -0,0 +1,11 @@
+module GiftCloud
+  class Client
+    def initialize host
+      # TODO
+    end
+    
+    def sign_in user, pass
+      # TODO
+    end
+  end
+end
--- bin/gift-test-system/ruby/test_gift_cloud.rb
+++ bin/gift-test-system/ruby/test_gift_cloud.rb
@@ -5,67 +5,67 @@ require_relative 'client'
 RSpec.describe GiftCloud::Client do
   # AUTHENTICATION ===================================
   it 'signs in' do
-    # skip 'not implemented'
+    skip 'not implemented'
   end
   
   it 'signs out' do
-    # skip 'not implemented'
+    skip 'not implemented'
   end
   # ==================================================
   
   # PROJECT ==========================================
   it 'creates a new project' do
-    # skip 'not implemented'
+    skip 'not implemented'
   end
   
   it 'does not re-create an existing project' do
-    # skip 'not implemented'
+    skip 'not implemented'
   end
   
   it 'lists all projects accessible to user' do
-    # skip 'not implemented'
+    skip 'not implemented'
   end
   
   it 'does not list any project not accessible to user' do
-    # skip 'not implemented'
+    skip 'not implemented'
   end
   # ==================================================
   
   # SUBJECT ==========================================
   it 'creates a new subject' do
-    # skip 'not implemented'
+    skip 'not implemented'
   end
   
   it 'does not re-create an existing subject' do
-    # skip 'not implemented'
+    skip 'not implemented'
   end
   
   it 'lists all subjects in a project' do
-    # skip 'not implemented'
+    skip 'not implemented'
   end
   # ==================================================
   
   # UPLOAD ===========================================
   it 'uploads zipped DICOM studies of a subject to a new session' do
-    # skip 'not implemented'
+    skip 'not implemented'
   end
   
   it 'uploads zipped DICOM studies of a subject to an existing session' do
-    # skip 'not implemented'
+    skip 'not implemented'
   end
   # ==================================================
   
   # PSEUDONYM ========================================
   it 'creates a new pseudonym for a subject' do
-    # skip 'not implemented'
+    skip 'not implemented'
   end
   
   it 'retrieves subject corresponding to an existing pseudonym' do
-    # skip 'not implemented'
+    skip 'not implemented'
   end
   
   it 'does not re-create an existing pseudonym' do
-    # skip 'not implemented'
+    skip 'not implemented'
   end
   # ==================================================
 end

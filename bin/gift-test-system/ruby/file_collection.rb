c24e43dd362defa540fac886acca552a6ae504ff
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2015-03-04 09:53:58 +0000

    comparison of a set of files
    Former-commit-id: 4e7f59706799d59bbee2b0885070ca58d81ceec3

--- bin/gift-test-system/ruby/file_collection.rb
+++ bin/gift-test-system/ruby/file_collection.rb
@@ -1,17 +1,28 @@
+require 'fileutils'
+
 module GiftCloud
   class FileCollection
     def initialize filenames
-      # TODO
+      @files = Array.new
+      filenames.each do |filename|
+        @files << File.new( filename, 'r' )
+      end
     end
     
     def match? filenames
-      # TODO
-      false
+      filenames.each do |filename|
+        return false unless include? filename
+      end
+      return true
     end
     
     def include? filename
-      # TODO
-      false
+      other_file = File.new( filename, 'r' )
+      @files.each do |file|
+        next unless FileUtils::cmp( file, other_file )
+        return true
+      end
+      return false
     end
   end
 end
